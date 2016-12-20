5a00609c2ad15d94fbda3168b1ce108bfabb64c5
teng <yangteng0813@gmail.com>
2016-12-19 23:58:18 -0500

    12.19

--- bigint/BigInt.java
+++ bigint/BigInt.java
@@ -1,26 +1,32 @@
 class BigInt{
-	String number;
+    String number;
 
-	public static void main(string[] args){
-		number = 123;
-		BigInt sol = new BigInt();
-		Stirng answer = sol.addBigInt(7);
-		System.out.println(answer);
-	}
+    public static void main(String[] agrs){
+        String number = "193";
+        String add = "8";
+        BigInt sol = new BigInt(number);
+        String answer = sol.addBigInt(add);
+        System.out.println(answer);
+    }
 
-	public BigInt(String number){ 
-		this.number = number;
-	}
-	public String addBigInt(String toAdd){
-		int len1  = number.length();
-		int len2 = toAdd.length();
-		while( len1 >= 0 && len2 >= 0 )
-			int carry = 0;
-			int number1 = Integer.parseInt(number.charAt(len1));
-			int number2 = Integer.parseInt(toAdd.charAt(len2));
-			res = (number1 + number2 + carry) % 10;
-			carry = (number1 + number2 + carry) / 10;
-			len1--;
-			len2--;
-	} 
+    public BigInt(String number){
+        this.number = number;
+    }
+    public String addBigInt(String toAdd){
+        int len1  = number.length() -1;
+        int len2 = toAdd.length() - 1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while( len1 >= 0 || len2 >= 0 ){
+            int res = 0 ;
+            if(len1 >= 0 ) res += number.charAt(len1--) - '0';
+            if(len2 >= 0 ) res += toAdd.charAt(len2--) - '0';
+            res += carry;
+            int pos = res % 10;
+            sb.append(pos);
+            carry = res / 10;
+        }
+        if(carry != 0 ) sb.append(carry);
+        return sb.reverse().toString();
+    }
 }

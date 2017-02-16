89e0497174d0df127ab495c74d784d162c0f481b
teng <yangteng0813@gmail.com>
2017-02-16 13:59:00 -0500

    2.16

---
+++ 140. Word Break II/Solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        if( s.length() == 0 ){
+            return new ArrayList<String>();
+        }
+        return helper(s,wordDict, new HashMap<String, List<String>>());
+    }
+    public List<String> helper(String s, List<String> wordDict, HashMap<String, List<String>> map){
+        if( map.containsKey(s)){
+            return map.get(s);
+        }
+        List<String> res = new ArrayList<>();
+        for( String word : wordDict ){
+            if( s.startsWith(word)){
+                if( word.length() == s.length() ){
+                    res.add(word);
+                    continue;
+                }
+                List<String> sublist = helper(s.substring(word.length()),wordDict,map);
+                for( String sub : sublist ){
+                    res.add(word + " " + sub);
+                }
+            }
+        }
+        map.put(s, res);
+        return res;
+    }
+}

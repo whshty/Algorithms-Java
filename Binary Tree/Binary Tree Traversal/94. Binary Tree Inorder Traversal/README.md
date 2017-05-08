8f0e52865ca788aebc29e6c2d78baf5d2a4f0a95
teng <yangteng0813@gmail.com>
2017-05-08 15:37:36 -0400

    Updated 94

--- Binary Tree/Binary Tree Traversal/94. Binary Tree Inorder Traversal/README.md
+++ Binary Tree/Binary Tree Traversal/94. Binary Tree Inorder Traversal/README.md
@@ -1,14 +1,23 @@
 # 94. Binary Tree Inorder Traversal
-## Idea (Stack)
+## Stack
+### Idea 
 * Left, Root, Right
 * Depth first to the left to store every node is a stack
 * The very first node is our start node
 * Check node's right first, the node maybe the root for other nodes
 * If the right is null, we can go to the parent node( stack pop)
 * If the right is not null, we need to store the right node in stack and travel to next very left
-* Use a flag to mark the finish status 
 
-
-## Time & Space 
+### Time & Space 
 * O(n)
 * O(n)
+
+## Recursion
+### Idea
+* DFS
+* If the current very left node has right child node, we need to consider it as parent node and go the right subtree
+
+### Time & Space
+* o(n)
+* o(n) + o(h)
+* o(h) is stack space, o(h) = o(logn) is the tree is balanced
--- Binary Tree/Binary Tree Traversal/94. Binary Tree Inorder Traversal/Solution.java
+++ Binary Tree/Binary Tree Traversal/94. Binary Tree Inorder Traversal/Solution.java
@@ -1,26 +1,39 @@
+// Stack
 public class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer>  res = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<TreeNode>();
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
         
         TreeNode node = root;
-        boolean isFinished = false;
-        
-        while( !isFinished ){
-            if( node != null){
+        while( node != null || !stack.isEmpty()){
+            while( node != null ){
                 stack.push(node);
                 node = node.left;
             }
-            else {
-                if( stack.isEmpty()){
-                    isFinished = true;
-                } else {
-                    node = stack.pop();
-                    res.add(node.val);
-                    node = node.right;
-                }
-            }
+            node = stack.pop();
+            res.add(node.val);
+            node = node.right;
         }
         return res;
+    }
+}
+
+// Recursion
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root,res);
+        return res;
+    }
+    public void helper(TreeNode node, List<Integer> res){
+        if( node != null ){
+            if ( node.left != null ){
+                helper(node.left,res);
+            }
+            res.add(node.val);
+            if( node.right != null ){
+                helper(node.right,res);
+            }    
+        }
     }
 }

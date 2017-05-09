507b9bc086acdff91d210148ec2785e9877a89ab
teng <yangteng0813@gmail.com>
2017-05-09 15:27:42 -0400

    5.9

---
+++ Binary Tree/Binary Tree Traversal/107. Binary Tree Level Order Traversal II/README.md
@@ -0,0 +1,9 @@
+# 102. Binary Tree Level Order Traversal
+## Idea
+* Just like the normal level order traversal
+* Use queue
+* add temp list to the start point
+
+## Time & Space
+* O(n), n is the number of nodes
+* O(n)
---
+++ Binary Tree/Binary Tree Traversal/107. Binary Tree Level Order Traversal II/Solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root ==null) return res;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while( !queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i = 0 ; i < size ; i++){
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if(node.left!=null) queue.add(node.left);
+                if(node.right!=null) queue.add(node.right);
+            }
+            res.add(0,list);
+        }
+        return res;
+    }
+}

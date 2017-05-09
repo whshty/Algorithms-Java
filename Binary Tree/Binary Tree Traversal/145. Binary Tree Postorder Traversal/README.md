e7dc20808d568aced14a4d9fd4b89f2295130a78
teng <yangteng0813@gmail.com>
2017-05-09 16:27:34 -0400

    5.9

---
+++ Binary Tree/Binary Tree Traversal/145. Binary Tree Postorder Traversal/README.md
@@ -0,0 +1,17 @@
+# 145. Binary Tree Postorder Traversal
+## Stack
+### Idea
+* Use stack to store every node
+* Construct the result from the end (root,right,left)
+
+### Time & Space
+* o(n) & o(n)
+
+## Recursion
+### Idea
+* Check left, check right and add current value
+
+
+### Time & Space
+* o(n)
+* o(n) + o(h)
--- Binary Tree/Binary Tree Traversal/145. Binary Tree Postorder Traversal/Solution.java
+++ Binary Tree/Binary Tree Traversal/145. Binary Tree Postorder Traversal/Solution.java
@@ -1,19 +1,31 @@
 public class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-	    LinkedList<Integer> res = new LinkedList<>();
-	    Stack<TreeNode> stack = new Stack<>();
-	    if (root == null) return res;
-	    stack.push(root);
-	    while (!stack.isEmpty()) {
-		    TreeNode cur = stack.pop();
-		    res.addFirst(cur.val);
-		    if (cur.left != null) {
-			    stack.push(cur.left);
-		    }
-		    if (cur.right != null) {
-			    stack.push(cur.right);
-		    } 
-	    }
-	    return res;
+        LinkedList<Integer> res = new ArrayList<>();
+        if( root == null ) return res;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        
+        while( !stack.isEmpty()){
+            TreeNode node = stack.pop();
+            res.addFirst(node.val);
+            if( node.left != null ) stack.push(node.left);
+            if( node.right !=null ) stack.push(node.right);
+        }
+        return res;
+    }
+}
+
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null ) return res;
+        helper(root,res);
+        return res;
+        
+    }
+    public void helper(TreeNode node, List<Integer> res){
+        if( node.left != null ) helper(node.left,res);
+        if( node.right != null ) helper(node.right,res);
+        res.add(node.val);
     }
 }

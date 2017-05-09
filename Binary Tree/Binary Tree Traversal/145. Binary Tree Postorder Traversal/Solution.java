a666cf76e2a3b8f50ce5b07398ca1c692f620668
teng <yangteng0813@gmail.com>
2017-05-09 16:01:37 -0400

    5.9

---
+++ Binary Tree/Binary Tree Traversal/145. Binary Tree Postorder Traversal/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+	    LinkedList<Integer> res = new LinkedList<>();
+	    Stack<TreeNode> stack = new Stack<>();
+	    if (root == null) return res;
+	    stack.push(root);
+	    while (!stack.isEmpty()) {
+		    TreeNode cur = stack.pop();
+		    res.addFirst(cur.val);
+		    if (cur.left != null) {
+			    stack.push(cur.left);
+		    }
+		    if (cur.right != null) {
+			    stack.push(cur.right);
+		    } 
+	    }
+	    return res;
+    }
+}

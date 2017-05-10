955c31b3f41d83c67f6cf0259cf7b95bec5d159b
teng <yangteng0813@gmail.com>
2017-05-09 21:12:04 -0400

    5.9

--- Binary Tree/Binary Tree Traversal/314. Binary Tree Vertical Order Traversal/README.md
+++ Binary Tree/Binary Tree Traversal/314. Binary Tree Vertical Order Traversal/README.md
@@ -1,6 +1,10 @@
 # 314. Binary Tree Vertical Order Traversal
-## Idea
-* 
+### Idea 
+* Use a HashMap to store column value and related result list
+* Use a HashMap to store the TreeNode and the according cloumn value
+* Map's key is column, we assume the root column is zero, the left node will minus 1 ,and the right node will plus 1
 
-## Time & Space
-* 
+
+### Time & Space 
+* o(n)
+* o(n)
--- Binary Tree/Binary Tree Traversal/314. Binary Tree Vertical Order Traversal/Solution.java
+++ Binary Tree/Binary Tree Traversal/314. Binary Tree Vertical Order Traversal/Solution.java
@@ -1,38 +1,38 @@
 public class Solution {
     public List<List<Integer>> verticalOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
-        if (root == null) {
-            return res;
-        }
-       //map's key is column, we assume the root column is zero, the left node will minus 1 ,and the right node will plus 1
-        HashMap<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
+        if( root == null ) return res;
+        HashMap<Integer,ArrayList<Integer>> map = new HashMap<>();
+        HashMap<TreeNode,Integer> weightMap = new HashMap<>();
         Queue<TreeNode> queue = new LinkedList<>();
-       //use a HashMap to store the TreeNode and the according cloumn value
-        HashMap<TreeNode, Integer> weight = new HashMap<TreeNode, Integer>();
-        queue.offer(root);
-        weight.put(root, 0);
+        queue.add(root);
+        weightMap.put(root,0);
         int min = 0;
-        while (!queue.isEmpty()) {
+        
+        while( !queue.isEmpty()){
             TreeNode node = queue.poll();
-            int w = weight.get(node);
-            if (!map.containsKey(w)) {
-                map.put(w, new ArrayList<>());
-            }
-            map.get(w).add(node.val);
-            if (node.left != null) {
+            int weight = weightMap.get(node);
+            
+            map.putIfAbsent(weight,new ArrayList<>());
+            
+            map.get(weight).add(node.val);
+            
+            if( node.left != null){
                 queue.add(node.left);
-                weight.put(node.left, w - 1);
-            } 
-            if (node.right != null) {
-                queue.add(node.right);
-                weight.put(node.right, w + 1);
+                weightMap.put(node.left,weight-1);
             }
-            //update min ,min means the minimum column value, which is the left most node
-            min = Math.min(min, w);
+            
+            if( node.right != null ){
+                queue.add(node.right);
+                weightMap.put(node.right,weight+1);
+            }
+            min = Math.min(min,weight);
         }
-        while (map.containsKey(min)) {
+        
+        while( map.containsKey(min)){
             res.add(map.get(min++));
         }
+        
         return res;
     }
 }

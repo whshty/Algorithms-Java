2854c34d4c5285b8b5f6c0402bd0718537f3fbfe
teng <yangteng0813@gmail.com>
2017-05-10 23:18:35 -0400

    5.10

--- Binary Tree/Binary Tree Construcion/106. Construct Binary Tree from Inorder and Postorder Traversal/Solution.java
+++ Binary Tree/Binary Tree Construcion/106. Construct Binary Tree from Inorder and Postorder Traversal/Solution.java
@@ -9,20 +9,19 @@
  */
 public class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        return build(inorder,inorder.length-1,0,postorder,postorder.length-1);
+        if(inorder.length == 0 || postorder.length == 0 ) return null;
+        return build(inorder,0,inorder.length-1,postorder,postorder.length-1);
     }
-    private TreeNode build(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart){
-        if( inEnd > inStart ) return null;
-        TreeNode root = new TreeNode(postorder[postStart]);
+    private TreeNode build(int[] inorder, int inStart, int inEnd, int[] postorder, int postEnd){
+        if( inStart >  inEnd) return null;
+        TreeNode root = new TreeNode(postorder[postEnd]);
         if( inEnd == inStart ) return root;
         int i = 0;
-        for( i = inStart; i >= inEnd; i -- ){
+        for( i = inEnd; i >= inStart; i -- ){
             if( inorder[i] == root.val) break;
         }
-        
-        root.right = build(inorder,inStart,i+1,postorder,postStart-1);
-        root.left = build(inorder,i-1,inEnd,postorder,postStart-(inStart-i)-1);
-        
+        root.left = build(inorder,inStart,i-1,postorder,postEnd-(inEnd-i)-1);
+        root.right = build(inorder,i+1,inEnd,postorder,postEnd-1);
         return root;
     }
 }

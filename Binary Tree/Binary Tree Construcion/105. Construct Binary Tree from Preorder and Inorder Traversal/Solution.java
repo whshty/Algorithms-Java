1b6789fccd1a39781e5bc2814f5a4cf3195596df
teng <yangteng0813@gmail.com>
2017-05-12 18:14:43 -0400

    5.12

--- Binary Tree/Binary Tree Construcion/105. Construct Binary Tree from Preorder and Inorder Traversal/Solution.java
+++ Binary Tree/Binary Tree Construcion/105. Construct Binary Tree from Preorder and Inorder Traversal/Solution.java
@@ -7,39 +7,29 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+
 public class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if(preorder.length!=inorder.length) return null;
-        return build(preorder, 0, preorder.length-1, inorder, 0, inorder.length-1);
+        if( preorder.length != inorder.length ) return null;
+        if( preorder.length == 0 || inorder.length == 0 ) return null;
+        return helper(preorder,0,preorder.length-1,inorder,0,inorder.length-1);
     }
     
-    public TreeNode build(int[] preorder,int preStart,int preEnd,int[] inorder, int inStart , int inEnd){
+    public TreeNode helper(int[]preorder, int preStart, int preEnd, int inorder[], int inStart, int inEnd ){
         if( preStart > preEnd || inStart > inEnd ) return null;
-        
+    
         TreeNode root = new TreeNode(preorder[preStart]);
-        
         int inorderRoot = inStart;
-        
-        for(int i = inStart ; i <= inEnd ; i++){
+        for( int i = inStart ; i <= inEnd ; i++ ){
             if( inorder[i] == root.val){
                 inorderRoot = i;
                 break;
             }
         }
         
-        int leftTreeLen = inorderRoot-inStart;
-        
-        root.left = build(preorder,preStart+1,preStart+leftTreeLen,inorder,inStart,inorderRoot-1);
-        root.right = build(preorder,preStart+leftTreeLen+1,preEnd,inorder,inorderRoot+1,preEnd);
+        int leftTreeLen = inorderRoot - inStart;
+        root.left = helper(preorder,preStart+1,preStart+leftTreeLen,inorder,inStart,inorderRoot-1);
+        root.right = helper(preorder,preStart+leftTreeLen+1,preEnd,inorder,inorderRoot+1,inEnd);
         
         return root;
     }

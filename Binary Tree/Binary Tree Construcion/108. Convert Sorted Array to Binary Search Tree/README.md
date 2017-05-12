7f05ae6ebd4bb5c8ae93d5b7e406168b20c20f4d
teng <yangteng0813@gmail.com>
2017-05-12 18:34:43 -0400

    5.12

---
+++ Binary Tree/Binary Tree Construcion/108. Convert Sorted Array to Binary Search Tree/README.md
@@ -0,0 +1,7 @@
+# 108. Convert Sorted Array to Binary Search Tree
+## Recursion
+### Idea
+* Use the mid value for root in this subtree for every recursion 
+
+## Time 
+* o(n)
--- Binary Tree/Binary Tree Construcion/108. Convert Sorted Array to Binary Search Tree/Solution.java
+++ Binary Tree/Binary Tree Construcion/108. Convert Sorted Array to Binary Search Tree/Solution.java
@@ -1,15 +1,16 @@
 public class Solution {
     public TreeNode sortedArrayToBST(int[] nums) {
-    	if (nums == null || nums.length == 0) return null;
-    	return buildtree (nums, 0, nums.length - 1);
+        if(nums.length == 0) return null;
+        return helper(nums,0,nums.length-1);
     }
-	private TreeNode buildtree (int[] nums, int start, int end){
-		if (start > end) return null;
-		int mid = start + (end - start) / 2;
-		
-		TreeNode root = new TreeNode(nums[mid]);
-		root.left = buildtree(nums, start , mid - 1);
-		root.right = buildtree(nums, mid+1, end);
-		return root;
-	}
+    
+    public TreeNode helper(int[]nums,int start, int end){
+        if( start > end ) return null;
+        int mid = start + ( end - start ) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums,start,mid-1);
+        root.right = helper(nums,mid+1,end);
+        
+        return root;
+    }
 }

b1ca586d7de131b1e0d41eb25c927d871169431b
teng <yangteng0813@gmail.com>
2017-05-16 12:53:57 -0400

    5.15

---
+++ Binary Tree/Binary Tree Search/110. Balanced Binary Tree/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if( root == null ) return true;
+        return height(root) != -1;
+    }
+    private int height(TreeNode root){
+        if( root == null ) return 0;
+        int lh = height(root.left);
+        int rh = height(root.right);
+        if( Math.abs(lh - rh ) > 1  || rh == -1  ||  lh == -1  ) return -1;
+        return Math.max(lh,rh) + 1;
+    }
+}

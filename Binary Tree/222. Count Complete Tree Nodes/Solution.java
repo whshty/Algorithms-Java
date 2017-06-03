8255296151e20de9b43d46e49597d2b966b56222
teng <yangteng0813@gmail.com>
2017-06-03 14:59:41 -0700

    6.3

---
+++ Binary Tree/222. Count Complete Tree Nodes/Solution.java
@@ -0,0 +1,21 @@
+// Brute force 
+public class Solution {
+    public int countNodes(TreeNode root) {
+        if( root == null ) return 0;
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
+
+// Recursion 
+public class Solution {
+    public int countNodes(TreeNode root) {
+        int treeHeight = countHeight(root);
+        if( treeHeight < 0 ) return 0;
+        if( countHeight(root.right) == treeHeight-1 ) return (1 << treeHeight) + countNodes(root.right);
+        else return (1 << treeHeight-1) + countNodes(root.left);
+    }
+    public int countHeight(TreeNode root){
+        if( root == null) return -1;
+        return 1 + countHeight(root.left);
+    }
+}

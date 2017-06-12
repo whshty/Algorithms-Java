b5e9d8c5e951d41be15ce0b5b971abe9e395526c
teng <yangteng0813@gmail.com>
2017-06-11 18:42:01 -0700

    6.11

--- Binary Search/69. Sqrt(x)/Solution.java
+++ Binary Search/69. Sqrt(x)/Solution.java
@@ -1,18 +1,16 @@
 public class Solution {
     public int mySqrt(int x) {
-        // corner case
         if( x == 0 ) return 0;
-        // binary search
-        int left = 0 , right = Integer.MAX_VALUE;
-        while ( true ){
-            int mid = left + ( right - left ) / 2;
-            if( mid > x /mid ){
+        int left = 0, right = Integer.MAX_VALUE;
+        
+        while(true){
+            int mid = ( left + right ) >>> 1 ;
+            if( mid > x / mid ){
                 right = mid - 1;
             } else {
-                if( mid + 1 > x /( mid + 1)) return mid;
+                if( mid + 1 > x / ( mid + 1 ) ) return mid;
                 left = mid + 1;
             }
         }
-        
     }
 }

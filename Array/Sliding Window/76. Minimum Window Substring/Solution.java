d795ecad3ad57972a077e546326752b9625956b3
teng <yangteng0813@gmail.com>
2017-06-12 22:02:46 -0700

    6.12

---
+++ Array/Sliding Window/76. Minimum Window Substring/Solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public String minWindow(String s, String t) {
+        if( s== null ||  t.length() > s.length() ) return "";
+        int[] freq = new int[128];
+        int start = 0, end = 0;
+        int minStart = 0;
+        int count = t.length();
+        int minLen = Integer.MAX_VALUE;
+        for( char ch : t.toCharArray()){
+            freq[ch]++;
+        }
+        while(end < s.length()){
+            if( freq[s.charAt(end)] > 0) {
+                count--;
+            }
+            freq[s.charAt(end)]--;
+            end++;
+            while(count == 0 ){
+                if (end - start < minLen) {
+				    minStart = start;
+				    minLen = end - start;
+			    }
+                freq[s.charAt(start)]++;
+                if(freq[s.charAt(start)] > 0) count++;
+                start++;
+            }
+        }
+        return (minLen==Integer.MAX_VALUE) ? "" : s.substring(minStart,minStart+minLen);
+    }
+}

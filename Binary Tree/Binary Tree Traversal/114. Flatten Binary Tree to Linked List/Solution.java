eb0ca3a176540f0ec7968e67fb78100310622260
teng <yangteng0813@gmail.com>
2017-06-20 21:27:02 -0700

    6.20

--- Binary Tree/Binary Tree Traversal/114. Flatten Binary Tree to Linked List/Solution.java
+++ Binary Tree/Binary Tree Traversal/114. Flatten Binary Tree to Linked List/Solution.java
@@ -13,3 +13,24 @@ public class Solution {
         }
     }
 }
+
+
+public class Solution {
+    public void flatten(TreeNode root) {
+        if( root == null ) return;
+        flatten(root.left);
+        flatten(root.right);
+        
+        TreeNode right = root.right;
+        
+        if( root.left != null ){
+            root.right = root.left;
+            root.left = null;
+            
+            while( root.right != null ){
+                root = root.right;
+            }
+            root.right = right;
+        }
+    }
+}

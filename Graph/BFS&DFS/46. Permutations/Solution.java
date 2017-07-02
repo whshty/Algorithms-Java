f0c80161d3a4f81092c6b2ce92fbe39eae61c28b
teng <yangteng0813@gmail.com>
2017-07-02 16:28:02 -0700

    7.2

--- Graph/BFS&DFS/46. Permutations/Solution.java
+++ Graph/BFS&DFS/46. Permutations/Solution.java
@@ -1,3 +1,7 @@
+// bfs
+// time complexity 
+// time O(n!)
+// space O(n!)
 public class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
@@ -16,5 +20,32 @@ public class Solution {
             res = nextRes;
         }
         return res;
+    }
+}
+
+// time O(n! * n )
+// space O(n)
+// no recursion stack
+
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if( nums == null || nums.length == 0 ) return res;
+        List<Integer> list = new ArrayList<>();
+        dfs(nums,res,list);
+        return res;
+    }
+    private void dfs(int nums[], List<List<Integer>> res, List<Integer> list ){
+        if( list.size() == nums.length ){
+            res.add(new ArrayList<Integer>(list));
+            return;
+        } 
+        for( int i = 0 ; i < nums.length ; i++ ){
+            if( !list.contains(nums[i])){
+                list.add(nums[i]);
+                dfs(nums,res,list);
+                list.remove(list.size()-1);
+            }
+        }
     }
 }

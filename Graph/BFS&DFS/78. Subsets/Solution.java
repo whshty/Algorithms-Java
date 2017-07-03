7962ffe0a8f553031887229eb24c40284704646e
teng <yangteng0813@gmail.com>
2017-07-02 18:50:01 -0700

    7.2

--- Graph/BFS&DFS/78. Subsets/Solution.java
+++ Graph/BFS&DFS/78. Subsets/Solution.java
@@ -1,5 +1,22 @@
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+        for( int num : nums ){
+            int size = res.size();
+            for( int i = 0 ; i < size ; i++ ){
+                List<Integer> list = new ArrayList<>(res.get(i));
+                list.add(num);
+                res.add(list);
+            }
+        }
+        return res;
+    }
+}
+
+
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         if(nums.length == 0) return res;
         helper(res,new ArrayList<>(),nums,0);

0ba9adfb161930e27c761ff40f65c363efb46d6b
teng <yangteng0813@gmail.com>
2017-07-03 18:32:18 -0700

    7.3

--- Graph/BFS&DFS/131. Palindrome Partitioning/Solution.java
+++ Graph/BFS&DFS/131. Palindrome Partitioning/Solution.java
@@ -1,31 +1,34 @@
 public class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> res = new ArrayList<>();
-        List<String> list = new ArrayList<>();
-        helper(s , 0 , res , list );
+        if( s == null || s.length() == 0 ) return res;
+        dfs(s,res,new ArrayList<>(),0);
         return res;
+        
     }
-    public void helper( String s , int pos , List<List<String>> res, List<String> list){
-        if( pos == s.length() ) {
-            res.add( new ArrayList<String>(list) );
-        } else {
-            for( int i = pos ; i < s.length(); i++ ){
-                if( isPalindrome(s , pos , i) ){
-                    list.add(s.substring(pos,i+1));
-                    helper( s , i+1 , res , list);
-                    list.remove( list.size() - 1 );
-                }
+    private void dfs(String s, List<List<String>> res, List<String> list, int pos){
+        if(  pos == s.length() ){
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for( int i = pos ; i < s.length() ; i++ ){
+            if( isPalindrome(s,pos,i) ){
+                list.add(s.substring(pos,i+1));
+                dfs(s,res,list,i+1);
+                list.remove(list.size()-1);
             }
         }
     }
-    public boolean isPalindrome( String s , int low , int high){
-        while( low < high ){
-            if( s.charAt(low) != s.charAt(high) ) {
+    private boolean isPalindrome(String s, int left, int right){
+        if ( s.length() == 1 ) return true;
+        while( left < right){
+            if( s.charAt(left) == s.charAt(right) ){
+                left++;
+                right--;
+            } else {
                 return false;
             }
-            low++;
-            high--;
         }
-        return true;
+        return true;        
     }
 }

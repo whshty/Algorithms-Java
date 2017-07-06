a5c7b232fd73da4d38678cb8a0856d8a74ee1d9c
teng <yangteng0813@gmail.com>
2017-07-05 20:10:03 -0700

    7.5

--- Memorized Search and DP /198. House Robber/Solution.java
+++ Memorized Search and DP /198. House Robber/Solution.java
@@ -1,5 +1,18 @@
 public class Solution {
     public int rob(int[] nums) {
+        int notVisitedPrevious = 0;
+        int visitedPrevious = 0 ;
+        for( int num : nums ){
+            int temp = notVisitedPrevious;
+            notVisitedPrevious = Math.max(notVisitedPrevious,visitedPrevious);
+            visitedPrevious = num + temp;
+        }
+        return Math.max(notVisitedPrevious,visitedPrevious);
+    }
+}
+
+public class Solution {
+    public int rob(int[] nums) {
         int[][] dp = new int[nums.length+1][2];
         for( int i = 1 ; i <= nums.length ; i++ ){
             dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1]);

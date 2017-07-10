1947818697d3f4c5f3731c25c4adf8b314a99226
teng <yangteng0813@gmail.com>
2017-07-09 17:59:24 -0700

    6.8

--- Memorized Search and DP /494. Target Sum/Solution.java
+++ Memorized Search and DP /494. Target Sum/Solution.java
@@ -13,3 +13,27 @@ public class Solution {
         return dfs(nums,target+nums[pos],pos+1,res) + dfs(nums,target-nums[pos],pos+1,res);
     }
 }
+
+
+public class Solution {
+    public int findTargetSumWays(int[] nums, int S) {
+        int res = 0;
+        if( nums == null || nums.length == 0 ) return res;
+        return dfs(nums,S,0,res, new HashMap<String,Integer>());
+    }
+    
+    private int dfs(int[] nums, int target, int pos ,int res, Map<String,Integer> map){
+        String key = pos + "-" + target;
+        if( map.containsKey(key)){
+            return map.get(key);
+        }
+        
+        if( pos == nums.length ){
+            if( target == 0 ) return 1;
+            else return 0;
+        }
+        int tempRes = dfs(nums,target+nums[pos],pos+1,res,map) + dfs(nums,target-nums[pos],pos+1,res,map);
+        map.put(key,tempRes);
+        return tempRes;
+    }
+}

75afa8b8a69db3afb53c133915b8208a38fb8394
teng <yangteng0813@gmail.com>
2017-07-23 16:25:48 -0700

    6.23

--- Array/Subarray/523. Continuous Subarray Sum/Solution.java
+++ Array/Subarray/523. Continuous Subarray Sum/Solution.java
@@ -1,19 +1,17 @@
 public class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
         if( nums == null || nums.length == 0 ) return false;
-        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        Map<Integer,Integer> map = new HashMap<>();
         map.put(0,-1);
-        int preSum = 0;
+        int sum = 0;
         for( int i = 0 ; i < nums.length ; i++ ){
-            preSum += nums[i];
-            if( k != 0 ) {
-                preSum %= k;
+            sum += nums[i];
+            if( k != 0 ) sum %= k;
+            Integer prePos = map.get(sum);
+            if( prePos != null ){
+                if(i - prePos > 1 ) return true;
             }
-            Integer pre = map.get(preSum);
-            if( pre != null ){
-                if( i - pre > 1 ) return true;
-            }
-            else map.put(preSum,i);
+            else map.put(sum,i);
         }
         return false;
     }

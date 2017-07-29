a91064e9ba5b58d9c85eec70098bda551a313b8c
teng <yangteng0813@gmail.com>
2017-07-29 13:41:17 -0700

    6.29

--- Binary Tree/Binary Tree Traversal/94. Binary Tree Inorder Traversal/Solution.java
+++ Binary Tree/Binary Tree Traversal/94. Binary Tree Inorder Traversal/Solution.java
@@ -1,3 +1,19 @@
+// Recursion
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root,res);
+        return res;
+    }
+    public void helper(TreeNode node, List<Integer> res){
+        if( node != null ){
+            if ( node.left != null ) helper(node.left,res);
+            res.add(node.val);
+            if( node.right != null ) helper(node.right,res);
+        }
+    }
+}
+
 // Stack
 public class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
@@ -18,22 +34,3 @@ public class Solution {
     }
 }
 
-// Recursion
-public class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        helper(root,res);
-        return res;
-    }
-    public void helper(TreeNode node, List<Integer> res){
-        if( node != null ){
-            if ( node.left != null ){
-                helper(node.left,res);
-            }
-            res.add(node.val);
-            if( node.right != null ){
-                helper(node.right,res);
-            }    
-        }
-    }
-}

6f7c8f4d3fd8e35ae6fec039c09e1070cd338df5
teng <yangteng0813@gmail.com>
2017-07-30 12:42:52 -0700

    6.30

--- String/Calculation/67. Add Binary/Solution.java
+++ String/Calculation/67. Add Binary/Solution.java
@@ -25,3 +25,39 @@ public class Solution {
         return sb.reverse().toString();
     }
 }
+
+
+import java.io.*;
+
+class myCode {
+    public static void main (String[] args) throws java.lang.Exception {
+        String res = addDecimal("1","19");
+        System.out.println(res);
+    }
+    public static String addDecimal(String a , String b){
+        if( a == null || a.length() == 0 ) return b;
+        if( b == null || b.length() == 0 ) return a;
+        int carry = 0 ;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while( i >= 0 || j >= 0 ){
+            int sum = 0;
+            int val;
+            sum += carry;
+            if( i >= 0 ) sum += a.charAt(i) - '0'; 
+            if( j >= 0 ) sum += b.charAt(j) - '0';
+            val = sum % 10;
+            sb.append(val);
+            carry = sum / 10;
+            i--;
+            j--;
+        }
+        if( carry != 0 ) sb.append(carry);
+        return sb.reverse().toString(); 
+    }
+}
+ }
+}

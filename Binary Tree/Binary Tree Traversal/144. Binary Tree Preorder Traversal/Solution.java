0a6665a77aeffd8fd3cc77ec719a0396ea390437
teng <yangteng0813@gmail.com>
2017-08-01 18:22:34 -0700

    8.1

--- Binary Tree/Binary Tree Traversal/144. Binary Tree Preorder Traversal/Solution.java
+++ Binary Tree/Binary Tree Traversal/144. Binary Tree Preorder Traversal/Solution.java
@@ -1,23 +1,3 @@
-// stack
-public class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        if( root == null ) return res;
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        stack.push(root);
-        
-        while( !stack.isEmpty()){
-            TreeNode node = stack.pop();
-            res.add(node.val);
-            if(node.right != null ) stack.push(node.right);
-            if(node.left != null ) stack.push(node.left);
-        }
-        return res;
-        
-    }
-}
-
-
 // recursion
 public class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
@@ -30,5 +10,24 @@ public class Solution {
         res.add(node.val);
         if(node.left != null )helper(node.left,res);
         if(node.right != null )helper(node.right,res);
+    }
+}
+
+// stack
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> deque = new LinkedList<>();
+        TreeNode node = root;
+        while( node != null || !deque.isEmpty()){
+            while( node != null ){
+                res.add(node.val);
+                deque.addFirst(node);
+                node = node.left;
+            }
+            node = deque.removeFirst(); 
+            node = node.right;
+        }
+        return res;
     }
 }

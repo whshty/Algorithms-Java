fe020c5037180c9b8f427591fdfdfd6362d831f2
teng <yangteng0813@gmail.com>
2017-08-07 18:52:34 -0700

    8.7

--- Binary Tree/Binary Search Tree/270. Closest Binary Search Tree Value/Solution.java
+++ Binary Tree/Binary Search Tree/270. Closest Binary Search Tree Value/Solution.java
@@ -1,27 +1,27 @@
 public class Solution {
     public int closestValue(TreeNode root, double target) {
-        int a = root.val;
-        TreeNode node = target < a ? root.left : root.right;
-        if ( node == null ) return a;
-        int b = closestValue(node, target);
-        return Math.abs(a - target) < Math.abs(b - target) ? a : b;
+        int cur = root.val;
+        if( target < cur ) {
+            root = root.left;
+        } else {
+            root = root.right;
+        }
+        if( root == null ) return cur;
+        int next = closestValue(root,target);
+        if( Math.abs( next - target ) < Math.abs( cur - target) ) return next;
+        else return cur;
     }
 }
-
 
 public class Solution {
     public int closestValue(TreeNode root, double target) {
         int res = root.val;
         while( root != null ){
-            if( Math.abs(target - root.val ) < Math.abs( target - res )){
+            if( Math.abs(root.val-target) < Math.abs(res-target)){
                 res = root.val;
             }
-            if( root.val > target ){
-                root = root.left;
-            }
-            else {
-                root = root.right;
-            }    
+            if( root.val > target ) root = root.left;
+            else root = root.right;
         }
         return res;
     }

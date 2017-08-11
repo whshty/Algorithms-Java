78e947fcf87d8b49e1ba279ca499e694e697936b
teng <yangteng0813@gmail.com>
2017-08-11 16:11:30 -0700

    8.11

--- Binary Tree/Bottom-Up/549. Binary Tree Longest Consecutive Sequence II/Solution.java
+++ Binary Tree/Bottom-Up/549. Binary Tree Longest Consecutive Sequence II/Solution.java
@@ -1,18 +1,18 @@
 public class Solution {
-    int max = 0;
-    
     public int longestConsecutive(TreeNode root) {
-        longestPath(root);
-        return max;
+        int[] res = new int[1];
+        if( root == null ) return res[0];
+        longestPath(root,res);
+        return res[0];
     }
     
-    public int[] longestPath(TreeNode root){
+    public int[] longestPath(TreeNode root, int[] res){
         if( root == null ){
             return new int[] {0,0};
         }
         int increase = 1, decrease = 1;
         if( root.left != null ){
-            int[] left = longestPath(root.left);
+            int[] left = longestPath(root.left,res);
             if( root.val == root.left.val + 1 ){
                 decrease = left[1] + 1;
             } else if ( root.val == root.left.val - 1 ){
@@ -20,15 +20,14 @@ public class Solution {
             }
         }
         if( root.right != null ){
-            int[] right = longestPath(root.right);
+            int[] right = longestPath(root.right,res);
             if( root.val == root.right.val + 1 ){
                 decrease = Math.max(decrease,right[1] + 1);
             } else if ( root.val == root.right.val - 1){
                 increase = Math.max( increase, right[0] + 1 );
             }
         }
-        max = Math.max(max,decrease+increase -1 );
+        res[0] = Math.max(res[0],decrease+increase -1 );
         return new int[]{increase,decrease};
-        
     }
 }

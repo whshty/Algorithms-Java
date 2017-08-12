fa4f930cd252027bf1efc3c0052ac6d702631f6e
teng <yangteng0813@gmail.com>
2017-08-11 20:16:02 -0700

    8.11

--- Binary Tree/Top-Down&Bottom-Up/111. Minimum Depth of Binary Tree/Solution.java
+++ Binary Tree/Top-Down&Bottom-Up/111. Minimum Depth of Binary Tree/Solution.java
@@ -1,5 +1,17 @@
 public class Solution {
     public int minDepth(TreeNode root) {
+        if( root == null ) return 0;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        if( left == 0 && right == 0 ) return 1;
+        else if(left == 0 ) return right + 1;
+        else if(right == 0 ) return left + 1;
+        return Math.min(left,right) + 1;
+    }
+}
+
+public class Solution {
+    public int minDepth(TreeNode root) {
         int res = 0;
         if( root == null ) return res;
         Queue<TreeNode> queue = new LinkedList<TreeNode>();

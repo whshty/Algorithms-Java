1d5e688b49b0d9ebd4cc75e490f8083dab139d7b
teng <yangteng0813@gmail.com>
2017-08-14 17:34:19 -0700

    8.14

--- Binary Tree/Bottom-Up/114. Flatten Binary Tree to Linked List/Solution.java
+++ Binary Tree/Bottom-Up/114. Flatten Binary Tree to Linked List/Solution.java
@@ -1,36 +1,41 @@
+// use stack to save node.
+// Preorder traversal 
 public class Solution {
     public void flatten(TreeNode root) {
-        if( root == null ) return;
+        if(root == null) return;
         Stack<TreeNode> stack = new Stack<TreeNode>();
         stack.push(root);
-        
         while(!stack.isEmpty()){
             TreeNode node = stack.pop();
-            if(node.right != null) stack.push(node.right);
-            if(node.left != null ) stack.push(node.left);
+            if(node.right!=null) stack.push(node.right);
+            if(node.left!=null) stack.push(node.left);
             if(!stack.isEmpty()) node.right = stack.peek();
             node.left = null;
         }
     }
 }
 
-
-public class Solution {
-    public void flatten(TreeNode root) {
-        if( root == null ) return;
-        flatten(root.left);
-        flatten(root.right);
+ public void flatten(TreeNode root) {
+     if(root == null)
+    return;
+    
+     flatten(root.left);
+     flatten(root.right);
+ 
+     // save current right for concatination
+     TreeNode right = root.right;
+ 
+     if(root.left != null) {
+     
+         // step 1: concatinate root with left flatten subtree
+        root.right = root.left;
+        root.left = null; // set left to null
+    
+        // step 2: move to the end of new added flatten subtree
+        while(root.right != null)
+            root = root.right;
         
-        TreeNode right = root.right;
-        
-        if( root.left != null ){
-            root.right = root.left;
-            root.left = null;
-            
-            while( root.right != null ){
-                root = root.right;
-            }
-            root.right = right;
-        }
-    }
-}
+        // step 3: contatinate left flatten subtree with flatten right subtree  
+        root.right = right;
+     }
+ }

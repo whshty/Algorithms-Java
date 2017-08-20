3022d1ad4cdb08d8fb877e276bf7a142db8d74a0
teng <yangteng0813@gmail.com>
2017-08-20 14:38:30 -0700

    8.20

--- Graph/Topological Sort/269. Alien Dictionary/Solution.java
+++ Graph/Topological Sort/269. Alien Dictionary/Solution.java
@@ -2,47 +2,48 @@ class Solution {
     public String alienOrder(String[] words) {
         Map<Character, Set<Character>> map=new HashMap<Character, Set<Character>>();
         Map<Character, Integer> degree=new HashMap<Character, Integer>();
-        String result="";
-        if(words==null || words.length==0) return result;
+        StringBuilder sb = new StringBuilder();
+        if (words == null || words.length == 0 ) return sb.toString();
         for(String s: words){
             for(char c: s.toCharArray()){
                 degree.put(c,0);
             }
         }
-        for(int i=0; i<words.length-1; i++){
-            String cur=words[i];
-            String next=words[i+1];
-            int length=Math.min(cur.length(), next.length());
-            for(int j=0; j<length; j++){
-                char c1=cur.charAt(j);
-                char c2=next.charAt(j);
-                if(c1!=c2){
-                    Set<Character> set=new HashSet<Character>();
-                    if(map.containsKey(c1)) set=map.get(c1);
-                    if(!set.contains(c2)){
-                        set.add(c2);
-                        map.put(c1, set);
-                        degree.put(c2, degree.get(c2)+1);
+        for(int i = 0; i < words.length-1; i++){
+            String one = words[i];
+            String two = words[i+1];
+            int length = Math.min(one.length(), two.length());
+            for(int j = 0 ; j < length; j++){
+                char ch1 = one.charAt(j);
+                char ch2 = two.charAt(j);
+                if( ch1 != ch2){
+                    Set<Character> set = new HashSet<Character>();
+                    if(map.containsKey(ch1)) set = map.get(ch1);
+                    if(!set.contains(ch2)){
+                        set.add(ch2);
+                        map.put(ch1, set);
+                        degree.put(ch2, degree.get(ch2)+1);
                     }
                     break;
                 }
             }
         }
-        Queue<Character> q=new LinkedList<Character>();
-        for(char c: degree.keySet()){
-            if(degree.get(c)==0) q.add(c);
+        Queue<Character> queue =new LinkedList<Character>();
+        for(char ch: degree.keySet()){
+            if(degree.get(ch)==0) queue.add(ch);
         }
-        while(!q.isEmpty()){
-            char c=q.remove();
-            result+=c;
-            if(map.containsKey(c)){
-                for(char c2: map.get(c)){
-                    degree.put(c2,degree.get(c2)-1);
-                    if(degree.get(c2)==0) q.add(c2);
+        while(!queue.isEmpty()){
+            char ch = queue.remove();
+            //result+=c;
+            sb.append(ch);
+            if(map.containsKey(ch)){
+                for(char ch2: map.get(ch)){
+                    degree.put(ch2,degree.get(ch2)-1);
+                    if(degree.get(ch2) == 0) queue.add(ch2);
                 }
             }
         }
-        if(result.length()!=degree.size()) return "";
-        return result;
+        if( sb.length() != degree.size()) return "";
+        return sb.toString();
     }
 }

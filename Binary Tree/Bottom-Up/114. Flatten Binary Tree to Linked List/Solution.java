67e51a1d738ba5134c3df22cd39eae22dc78e401
teng <yangteng0813@gmail.com>
2017-08-22 15:59:38 -0700

    8.22

--- Binary Tree/Bottom-Up/114. Flatten Binary Tree to Linked List/Solution.java
+++ Binary Tree/Bottom-Up/114. Flatten Binary Tree to Linked List/Solution.java
@@ -1,3 +1,20 @@
+class Solution {
+    public void flatten(TreeNode root) {
+        if(  root == null ) return;
+        TreeNode right = root.right;
+        
+        flatten(root.left);
+        flatten(root.right);
+        
+        root.right = root.left;
+        root.left = null;
+        while( root.right != null ){
+            root = root.right;
+        }
+        root.right = right;
+    }
+}
+
 // use stack to save node.
 // Preorder traversal 
 public class Solution {
@@ -14,28 +31,3 @@ public class Solution {
         }
     }
 }
-
- public void flatten(TreeNode root) {
-     if(root == null)
-    return;
-    
-     flatten(root.left);
-     flatten(root.right);
- 
-     // save current right for concatination
-     TreeNode right = root.right;
- 
-     if(root.left != null) {
-     
-         // step 1: concatinate root with left flatten subtree
-        root.right = root.left;
-        root.left = null; // set left to null
-    
-        // step 2: move to the end of new added flatten subtree
-        while(root.right != null)
-            root = root.right;
-        
-        // step 3: contatinate left flatten subtree with flatten right subtree  
-        root.right = right;
-     }
- }

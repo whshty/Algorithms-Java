c8cb10e8423b09e87c079a632c15b05d121caef1
teng <yangteng0813@gmail.com>
2017-08-23 22:24:08 -0700

    8.23

--- Graph/Topological Sort/444. Sequence Reconstruction/Solution.java
+++ Graph/Topological Sort/444. Sequence Reconstruction/Solution.java
@@ -2,6 +2,30 @@ class Solution {
     public boolean sequenceReconstruction(int[] org, List<List<Integer>> seqs) {
         Map<Integer, Set<Integer>> map = new HashMap<>();
         Map<Integer, Integer> indegree = new HashMap<>();
+        initGraph(seqs,map,indegree);
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for (Integer key : indegree.keySet()) {
+            if (indegree.get(key) == 0) {
+                queue.offer(key);
+            }
+        }
+
+        int index = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            if (size > 1) return false;
+            int curr = queue.poll();
+            if (index == org.length || curr != org[index++]) return false;
+            for (int next : map.get(curr)) {
+                indegree.put(next, indegree.get(next) - 1);
+                if (indegree.get(next) == 0) queue.offer(next);
+            }
+        }
+        return index == org.length && index == map.size();
+    }
+    
+    private void initGraph(List<List<Integer>> seqs, Map<Integer, Set<Integer>> map, Map<Integer, Integer> indegree){
         for (List<Integer> seq : seqs) {
             if (seq.size() == 1) {
                 if (!map.containsKey(seq.get(0))) {
@@ -28,25 +52,8 @@ class Solution {
                 }
             }
         }
-
-        Queue<Integer> queue = new LinkedList<>();
-        for (Integer key : indegree.keySet()) {
-            if (indegree.get(key) == 0) {
-                queue.offer(key);
-            }
-        }
-
-        int index = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            if (size > 1) return false;
-            int curr = queue.poll();
-            if (index == org.length || curr != org[index++]) return false;
-            for (int next : map.get(curr)) {
-                indegree.put(next, indegree.get(next) - 1);
-                if (indegree.get(next) == 0) queue.offer(next);
-            }
-        }
-        return index == org.length && index == map.size();
     }
+    
+    
+    
 }

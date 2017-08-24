1851641611657307cbccb3b7fc0e6b1694afbaf1
teng <yangteng0813@gmail.com>
2017-08-23 22:18:24 -0700

    8.23

--- Graph/Topological Sort/444. Sequence Reconstruction/Solution.java
+++ Graph/Topological Sort/444. Sequence Reconstruction/Solution.java
@@ -1,43 +1,52 @@
 class Solution {
     public boolean sequenceReconstruction(int[] org, List<List<Integer>> seqs) {
-        Map<Integer, Set<Integer>> map = new HashMap<>(); 
-        int[] degree = new int[org.length];
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        Map<Integer, Integer> indegree = new HashMap<>();
+        for (List<Integer> seq : seqs) {
+            if (seq.size() == 1) {
+                if (!map.containsKey(seq.get(0))) {
+                    map.put(seq.get(0), new HashSet<>());
+                    indegree.put(seq.get(0), 0);
+                }
+            } else {
+                for (int i = 0; i < seq.size() - 1; i++) {
+                    int one = seq.get(i);
+                    int two = seq.get(i+1);
+                    if (!map.containsKey(one)) {
+                        map.put(one, new HashSet<>());
+                        indegree.put(one, 0);
+                    }
 
-        // for(int i = 1; i<=org.length; i++) map.put(i, new HashSet<>()); 
-       //cannot init map with org, has to do with seqs, to avoid case like [1], [], expected: false
-        
-        for(List<Integer> list: seqs){
-            if(list.size()==1) map.computeIfAbsent(list.get(0), k->new HashSet<Integer>()); //dont forget
-            for(int i = 1; i<list.size(); i++){
-                int pre = list.get(i-1);
-                int cur = list.get(i);
-                map.computeIfAbsent(pre, k->new HashSet<Integer>()); 
-                map.computeIfAbsent(cur, k->new HashSet<Integer>()); 
-                if(pre>org.length || cur>org.length || pre<1 || cur<1) return false; 
-                //dont forget. or degree array can "ArrayIndexOutOfBoundsException"
-                
-                if(!map.get(pre).contains(cur)){
-                    degree[cur-1]++; 
-                    map.get(pre).add(cur);
+                    if (!map.containsKey(two)) {
+                        map.put(two, new HashSet<>());
+                        indegree.put(two, 0);
+                    }
+
+                    if (map.get(one).add(two)) {
+                        indegree.put(two, indegree.get(two) + 1);
+                    }
                 }
             }
         }
-        
-        Queue<Integer> q = new LinkedList<>();
-        for(int i = 0; i<degree.length; i++){
-            if(degree[i]==0) q.add(i+1);
-        }
-        int index = 0;
-        while(!q.isEmpty()){
-            if(q.size()>1) return false; //check with org
-            int cur = q.poll(); 
-            if(org[index++] != cur) return false; //check with org
-            if(!map.containsKey(cur)) continue; //don't forget
-            for(int ii: map.get(cur)){
-                degree[ii-1]--; 
-                if(degree[ii-1]==0) q.add(ii); 
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (Integer key : indegree.keySet()) {
+            if (indegree.get(key) == 0) {
+                queue.offer(key);
             }
         }
-        return index == org.length && index==map.size();//has to check with map size
+
+        int index = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            if (size > 1) return false;
+            int curr = queue.poll();
+            if (index == org.length || curr != org[index++]) return false;
+            for (int next : map.get(curr)) {
+                indegree.put(next, indegree.get(next) - 1);
+                if (indegree.get(next) == 0) queue.offer(next);
+            }
+        }
+        return index == org.length && index == map.size();
     }
 }

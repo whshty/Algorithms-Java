4eb737580fb1bbfa047e1412ffdffa0767b59609
teng <yangteng0813@gmail.com>
2017-08-31 18:18:49 -0400

    8.31

--- Linear Structure/Interval/56. Merge Intervals/Solution.java
+++ Linear Structure/Interval/56. Merge Intervals/Solution.java
@@ -1,23 +1,25 @@
-public class Solution {
-    public List<Interval> merge(List<Interval> intervals) {
-        // corner case checked 
-        if( intervals.size() <= 1) return intervals;
-        List<Interval> res = new LinkedList<>();
-        // sort by starting point using an anonymous comparator
-        intervals.sort((a, b) -> Integer.compare(a.start, b.start));
+class Solution {
+    public List<Interval> merge(List<Interval> list) {
+        List<Interval> res = new ArrayList<>();
+        if( list.size() == 0 ) return res;
+        if( list.size() == 1 ) return list;
+        list.sort((a, b) -> Integer.compare(a.start, b.start));
         
-        int start = intervals.get(0).start;
-        int end = intervals.get(0).end;
-        for(Interval interval : intervals){
-            // overlapping
-            if(interval.start <= end) end = Math.max(end, interval.end);
-            else {
-                res.add(new Interval(start, end));
-                start = interval.start;
-                end = interval.end;
+        int start = list.get(0).start;
+        int end = list.get(0).end;
+        
+        for( Interval i : list ){
+            if( i.start <= end ){
+                end = Math.max(end,i.end);
+            } else {
+                res.add(new Interval(start,end));
+                start = i.start;
+                end = i.end;
             }
         }
-        res.add(new Interval(start, end));
-        return res;
+        res.add(new Interval(start,end));
+
+        return res; 
     }
+}
 }

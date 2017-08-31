c256a0138d3cb600dc5a8fbfeab56eb065861718
teng <yangteng0813@gmail.com>
2017-08-31 14:46:43 -0400

    8.31

--- Linear Structure/Calculation/43. Multiply Strings/Solution.java
+++ Linear Structure/Calculation/43. Multiply Strings/Solution.java
@@ -2,24 +2,22 @@ public class Solution {
     public String multiply(String num1, String num2) {
         int m = num1.length();
         int n = num2.length();
-        int[] res = new int[m+n];                                         
-        for( int i = m - 1 ; i >=0 ; i--){
-            for( int j = n - 1 ; j >= 0 ; j--){
-                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-                int p1 = i + j, p2 = i+j+1;
-                int sum = mul + res[p2];
-                
-                res[p1] += sum/10;
-                res[p2] = sum%10;
+        int[] res = new int[m+n];
+        
+        for( int i = m -1 ; i >= 0 ; i--){
+            for( int j = n - 1 ; j >= 0 ; j-- ){
+                int mul = (num1.charAt(i)-'0') * (num2.charAt(j)-'0');
+                int index1 = i + j , index2 = i + j + 1;
+                int sum = mul + res[index2];
+                res[index1] += sum/10;
+                res[index2] = sum%10;
             }
         }
         StringBuilder sb = new StringBuilder();
         for( int num : res ){
-            if(!(sb.length() == 0 && num == 0)){
-                sb.append(num);
-            }
-        }
-        if(sb.length() ==0 ) return "0";
-        return sb.toString();
+            if( sb.length() == 0 && num == 0 ) continue;
+            sb.append(num);
+        } 
+        return sb.length() == 0 ? "0" : sb.toString();
     }
 }

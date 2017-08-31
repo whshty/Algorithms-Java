40c9b4d0bf89ad3bb0aaaa7d6b597be6f195dbad
teng <yangteng0813@gmail.com>
2017-08-30 20:55:19 -0400

    8.30

--- Linear Structure/Subarray/560. Subarray Sum Equals K/Solution.java
+++ Linear Structure/Subarray/560. Subarray Sum Equals K/Solution.java
@@ -13,3 +13,20 @@ class Solution {
         return count;
     }
 }
+
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        if( nums == null || nums.length == 0 ) return 0;
+        int sum = 0;
+        int count = 0;
+        Map<Long,Integer> map = new HashMap<>();
+        map.put(new Long(0),1);
+        for( int i = 0 ; i < nums.length ; i++ ){
+            sum += nums[i];
+            if(map.containsKey(new Long(sum-k))) count += map.get(new Long(sum-k));
+            map.put(new Long(sum),map.getOrDefault(new Long(sum),0) + 1);
+        }
+        return count;
+    }
+}

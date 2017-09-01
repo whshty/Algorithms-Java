98a7fcb2812031414002e29ceeeafc7f9c8ac6a3
teng <yangteng0813@gmail.com>
2017-09-01 14:57:23 -0400

    9.1

--- Binary Search/278. First Bad Version/Solution.java
+++ Binary Search/278. First Bad Version/Solution.java
@@ -2,7 +2,7 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int start = 1, end = n;
         while (start < end) {
-            int mid = start + (end-start) / 2;
+            int mid = ( start + end ) >>> 1;
             if (!isBadVersion(mid)) start = mid + 1;
             else end = mid;            
         }        

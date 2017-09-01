6ffe52e1feead34b234c8fd12cc95c1629564cf0
teng <yangteng0813@gmail.com>
2017-09-01 15:42:12 -0400

    9.1

--- Memorized Search and DP /139. Word Break/Solution.java
+++ Memorized Search and DP /139. Word Break/Solution.java
@@ -13,19 +13,19 @@ class Solution {
     }
 }
 
-public class Solution {
+class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         int len = s.length();
-        boolean[] cache = new boolean[len+1];
-        cache[0] = true;
+        boolean[] dp = new boolean[len+1];
+        dp[0] = true;
         for( int i = 1 ; i < len + 1 ; i++){
            for( int j = 0 ; j < i ; j++ ){
-               if( cache[j] && wordDict.contains(s.substring(j,i))){
-                   cache[i] = true;
+               if( dp[j] && wordDict.contains(s.substring(j,i))){
+                   dp[i] = true;
                }
            }  
            
         }
-        return cache[len];
+        return dp[len];
     }
 }

23d49e4e8b15ef3e414d330e6d0c0927d6917169
teng <yangteng0813@gmail.com>
2017-09-04 13:56:19 -0400

    9.4

--- BFS&DFS/1D/282. Expression Add Operators/Solution.java
+++ BFS&DFS/1D/282. Expression Add Operators/Solution.java
@@ -2,34 +2,6 @@ public class Solution {
     public List<String> addOperators(String num, int target) {
         List<String> res = new ArrayList<String>();
         if(num == null || num.length() == 0) return res;
-        helper(res,"",num,target,0,0,0);
-        return res;
-    }
-    private void helper(List<String> res, String path, String num, int target, int pos, long tempRes, long multed){
-        if(pos == num.length()){
-            if(target == tempRes) res.add(path);
-            return;
-        }
-        for(int i = pos; i < num.length() ; i++ ){
-            if( i != pos && num.charAt(pos) == '0') break;
-            long cur = Long.parseLong(num.substring(pos,i+1));
-            if(pos == 0){
-                helper(res,path+cur,num,target,i+1,cur,cur);
-            } else{
-                helper(res,path+"+"+cur,num,target,i+1,tempRes+cur,cur);
-                helper(res,path+"-"+cur,num,target,i+1,tempRes-cur,-cur);
-                helper(res,path+"*"+cur,num,target,i+1,tempRes-multed+multed*cur,multed*cur);
-            }
-        } 
-    }
-}
-
-
-
-public class Solution {
-    public List<String> addOperators(String num, int target) {
-        List<String> res = new ArrayList<String>();
-        if(num == null || num.length() == 0) return res;
         helper(res,new StringBuilder(),num,target,0,0,0);
         return res;
     }

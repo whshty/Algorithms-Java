5f640fc6555a3547882b31d5cfb8aa7ae925bb8e
teng <yangteng0813@gmail.com>
2017-09-09 23:25:23 -0400

    9.9

--- Memorized Search and DP /518. Coin Change 2/Solution.java
+++ Memorized Search and DP /518. Coin Change 2/Solution.java
@@ -13,3 +13,33 @@ class Solution {
         return res;
     }
 }
+
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        if( coins == null || coins.length == 0 && amount == 0 ) return 1;
+        return helper(coins,0,amount, new HashMap<Node,Integer>());
+    }
+    public int helper(int[] nums, int pos , int amount, Map<Node,Integer> map){
+        if( amount < 0 ) return 0;
+        if( amount == 0 ) return 1;
+        Node temp = new Node(amount,pos);
+        if( map.containsKey(temp)) return map.get(amount);
+        
+        int res = 0;
+        for( int i = pos ; i < nums.length ; i++ ){
+            res += helper(nums,i,amount-nums[i],map);
+        }
+        map.put(new Node(amount,pos),res);
+        return res;
+    }
+}
+
+class Node {
+    int pos;
+    int amount;
+    Node(int p, int a){
+        pos = p;
+        amount = a;
+    }
+}

75b0b1fd4be975e70df0f1808968720b209c1f8f
teng <yangteng0813@gmail.com>
2017-09-11 12:27:15 -0400

    9.11

--- Linear Structure/Pointer/Pointers/88. Merge Sorted Array/Solution.java
+++ Linear Structure/Pointer/Pointers/88. Merge Sorted Array/Solution.java
@@ -2,22 +2,21 @@ public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m - 1;
         int j = n - 1;
-        int k = n + m - 1;
+        int index = n + m - 1;
         while ( i >= 0 && j >= 0 ){
             if ( nums1[i] > nums2[j]){
-                nums1[k] = nums1[i];
+                nums1[index] = nums1[i];
                 i--;
-                k--;
-            }
-            else {
-                nums1[k] = nums2[j];
+                index--;
+            } else {
+                nums1[index] = nums2[j];
                 j--;
-                k--;
+                index--;
             }
         }
         while ( j >= 0 ){
-            nums1[k] = nums2[j];
-            k--;
+            nums1[index] = nums2[j];
+            index--;
             j--;
         }
     }

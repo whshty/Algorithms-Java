b2780340877fb31a02505f17575068f74b841417
teng <yangteng0813@gmail.com>
2017-09-11 12:46:05 -0400

    9.11

---
+++ Linear Structure/Pointer/Single Pointer/283. Move Zeroes/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        if( nums == null || nums.length == 0 ) return;
+        int index = 0;
+        for( int i = 0 ; i < nums.length ; i++ ){
+            if( nums[i] != 0 ){
+                int temp = nums[index];
+                nums[index] = nums[i];
+                nums[i] = temp;
+                index++;
+            }
+        }
+    
+    }
+}

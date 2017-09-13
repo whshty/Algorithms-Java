1a11a8851f1644e3e47e31085c8c82086af3da88
teng <yangteng0813@gmail.com>
2017-09-13 14:29:15 -0400

    9.13

--- Linear Structure/Pointer/Palindrome/125. Valid Palindrome/Solution.java
+++ Linear Structure/Pointer/Palindrome/125. Valid Palindrome/Solution.java
@@ -17,3 +17,30 @@ public class Solution {
         return true;
     }
 }
+
+class myCode {
+    public static void main (String[] args) throws java.lang.Exception {
+        System.out.println(isPalindrome("cabeebac",0));
+    }
+    
+    public static boolean isPalindrome(String s, int count) {
+        if( s.isEmpty() ) return true;
+        int start = 0, end = s.length() -1;
+        
+        char left, right;
+        while(start <= end){
+            left = s.charAt(start);
+            right = s.charAt(end);
+
+                
+            if (Character.toLowerCase(left) != Character.toLowerCase(right)) {
+                if( count == 1 )return false;
+                return isPalindrome(s.substring(start+1,end+1),1) || isPalindrome(s.substring(start,end),1);
+            }
+            start++;
+            end--;
+            
+        }
+        return true;
+    }
+}

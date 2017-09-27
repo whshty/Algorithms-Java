160c965a4d1e96d1471aafb422a8808ada8999fc
teng <yangteng0813@gmail.com>
2017-09-27 17:46:15 -0400

    9.27

--- Linear Structure/Pointer/Pointers/633. Sum of Square Numbers/Solution.java
+++ Linear Structure/Pointer/Pointers/633. Sum of Square Numbers/Solution.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
         if( c < 0 ) return false;
+        if( c == 1 ) return true; 
         int start = 0 , end = (int)Math.sqrt(c);
         
         while( start <= end  ){
             int res = start * start + end * end;
             if( res ==  c )  return true;
-            else if ( c > res ) end--;
-            else start++;
+            else if ( c > res ) start++;
+            else end--;
         }
         return false;
     }

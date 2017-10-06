4067633db817f1478d5c7e9b5ea80bc07d0472ec
teng <yangteng0813@gmail.com>
2017-10-06 16:28:42 -0400

    10.6

--- Memorized Search and DP /304. Range Sum Query 2D - Immutable/Solution.java
+++ Memorized Search and DP /304. Range Sum Query 2D - Immutable/Solution.java
@@ -29,16 +29,16 @@ class NumMatrix {
         
         for(int i = 1; i <= m; i++){
             for(int j = 1; j <= n; j++){
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] -dp[i - 1][j - 1] + matrix[i - 1][j - 1] ;
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + matrix[i - 1][j - 1] ;
             }
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int iMin = Math.min(row1, row2);
-        int iMax = Math.max(row1, row2);
-        int jMin = Math.min(col1, col2);
-        int jMax = Math.max(col1, col2);
-        return dp[iMax + 1][jMax + 1] - dp[iMax + 1][jMin] - dp[iMin][jMax + 1] + dp[iMin][jMin];    
+        int rowStart = Math.min(row1, row2);
+        int rowEnd = Math.max(row1, row2);
+        int colStart = Math.min(col1, col2);
+        int colEnd = Math.max(col1, col2);
+        return dp[rowEnd + 1][colEnd + 1] - dp[rowEnd + 1][colStart] - dp[rowStart][colEnd + 1] + dp[rowStart][colStart];    
     }
 }

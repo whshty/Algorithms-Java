bf17eef3bc08738def5dca430598dbd6eedb23da
teng <yangteng0813@gmail.com>
2017-10-08 15:31:57 -0400

    10.8

---
+++ Linear Structure/Scan and Search/268. Missing Number/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int len = nums.length;
+        int sum = len * ( len + 1 ) >>>  1;
+        for( int i = 0 ; i < len ; i++ ){
+            sum -= nums[i];
+        }
+        return sum;
+    }
+}

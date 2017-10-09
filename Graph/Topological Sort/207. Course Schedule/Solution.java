72685577c7bd37a5593f410a27cc664a879e57e5
teng <yangteng0813@gmail.com>
2017-10-08 22:07:03 -0400

    10.8

--- Graph/Topological Sort/207. Course Schedule/Solution.java
+++ Graph/Topological Sort/207. Course Schedule/Solution.java
@@ -1,18 +1,15 @@
 public class Solution {
     public boolean canFinish(int numCourses, int[][] pre) {
-        Map<Integer,ArrayList<Integer>> map = new HashMap<>();
+        Map<Integer,List<Integer>> map = new HashMap<>();
         Queue<Integer> queue = new LinkedList<>();
         int[] indegree = new int[numCourses];
         int count = numCourses;
         
-        // for (int i = 0; i < numCourses; i++) {
-        //     map.put(i, new ArrayList<Integer>());
-        // }
         for( int i = 0 ; i < pre.length ; i++ ){
-            map.putIfAbsent(pre[i][0], new ArrayList<Integer>());
-            map.get(pre[i][0]).add(pre[i][1]);
-            //map.put(pre[i][0],map.getOrDefault(pre[i][0],new ArrayList<Integer>()).add(pre[i][1]));
-            indegree[pre[i][1]]++;
+            map.putIfAbsent(pre[i][1], new ArrayList<Integer>());
+            List<Integer> temp = new ArrayList<>();
+            map.get(pre[i][1]).add(pre[i][0]);
+            indegree[pre[i][0]]++;
         }
         for( int i = 0 ; i < numCourses ; i++ ){
             if( indegree[i] == 0 ){

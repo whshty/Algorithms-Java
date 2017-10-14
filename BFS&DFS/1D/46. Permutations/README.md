f9df7b9aaa80158be6bbfb926040cc7c4a09fc86
teng <yangteng0813@gmail.com>
2017-10-14 15:12:10 -0400

    10.14

--- BFS&DFS/1D/46. Permutations/README.md
+++ BFS&DFS/1D/46. Permutations/README.md
@@ -3,17 +3,17 @@
 * Space : O(n!)
 
 ```java
-public class Solution {
+class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         if( nums == null || nums.length == 0 ) return res;
         res.add(new ArrayList<Integer>());
         
         for( int i = 0 ; i < nums.length ; i++ ){
-            List<List<Integer>> nextRes = new ArrayList<List<Integer>>();
+            List<List<Integer>> nextRes = new ArrayList<>();
             for( List<Integer> list : res ){
                 for( int j = 0 ; j <= list.size() ; j++ ){
-                    List<Integer> nextList = new ArrayList<Integer>(list);
+                    List<Integer> nextList = new ArrayList<>(list);
                     nextList.add(j,nums[i]);
                     nextRes.add(nextList);
                 }
--- BFS&DFS/1D/46. Permutations/Solution.java
+++ BFS&DFS/1D/46. Permutations/Solution.java
@@ -2,17 +2,17 @@
 // time complexity 
 // time O(n!)
 // space O(n!)
-public class Solution {
+class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         if( nums == null || nums.length == 0 ) return res;
         res.add(new ArrayList<Integer>());
         
         for( int i = 0 ; i < nums.length ; i++ ){
-            List<List<Integer>> nextRes = new ArrayList<List<Integer>>();
+            List<List<Integer>> nextRes = new ArrayList<>();
             for( List<Integer> list : res ){
                 for( int j = 0 ; j <= list.size() ; j++ ){
-                    List<Integer> nextList = new ArrayList<Integer>(list);
+                    List<Integer> nextList = new ArrayList<>(list);
                     nextList.add(j,nums[i]);
                     nextRes.add(nextList);
                 }

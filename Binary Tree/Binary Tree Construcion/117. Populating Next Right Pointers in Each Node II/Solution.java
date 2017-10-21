8fc6c49b6e9700cf87c469b02ba1caa3649ced37
teng <yangteng0813@gmail.com>
2017-10-21 14:30:12 -0400

    10.21

--- Binary Tree/Binary Tree Construcion/117. Populating Next Right Pointers in Each Node II/Solution.java
+++ Binary Tree/Binary Tree Construcion/117. Populating Next Right Pointers in Each Node II/Solution.java
@@ -1,23 +1,39 @@
 public class Solution {
     public void connect(TreeLinkNode root) {
-        TreeLinkNode node = root;
-        TreeLinkNode dummy = new TreeLinkNode(-1);
+        // node moves to all nodes in tree in level order 
+        // needle keeps sewing together next level's children
+        // dummy is keeping track of start node of next level
+        TreeLinkNode node = root, dummy = new TreeLinkNode(-1);
         
-        while( node != null ){
+        // this loop is for different levels
+        while(node != null){ 
+            // cur is sewing next fields in current level
+            // first time in a level, it is same as dummy (with null next)
+            // but as soon as we get a non null child from node
+            // needle threads that child into its next, thus making that child as next dummy
             TreeLinkNode cur = dummy;
-            while( node != null ){
-                if( node.left != null ){
+        
+            // this loop moves node in current level
+            while(node != null){
+                if(node.left != null){
                     cur.next = node.left;
                     cur = cur.next;
                 }
-                if(node.right != null ){
+                if(node.right != null){
                     cur.next = node.right;
                     cur = cur.next;
                 }
-                node = node.next;
+                
+                node = node.next; // move node to next in same level, end up null at rightmost
             }
+            // current level ended in node being null
+            // take node from sentinel's next, which is next levels start
             node = dummy.next;
-            dummy.next = null;
+            
+            // levelhead.next grabbed into node above, it has been used
+            // so make it null so next time we dont grab again, 
+            // if all next lvl is null, it remains null prompting end of run
+            dummy.next = null;  
         }
     }
 }

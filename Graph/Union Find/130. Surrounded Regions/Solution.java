306f61aaebf7745a61beeebfd169a18d7f573c68
teng <yangteng0813@gmail.com>
2017-10-22 21:01:25 -0400

    10.22

--- Graph/Union Find/130. Surrounded Regions/Solution.java
+++ Graph/Union Find/130. Surrounded Regions/Solution.java
@@ -1,78 +1,50 @@
 public class Solution {
-    private int[] ids;
-    private int[] sizes;
-        
-    private int oEdge;
-    int m;
-    int n;
+    
+    int[] unionSet; // union find set
+    boolean[] hasEdgeO; // whether an union has an 'O' which is on the edge of the matrix
+    
     public void solve(char[][] board) {
-        if((m = board.length) == 0 || (n = board[0].length) == 0) return;
-        ids = new int[m*n];
-        sizes = new int[m*n];
-        Arrays.fill(sizes,1);
-        oEdge = -1;
-      
-        for( int i = 0 ; i < m ; i++ ){
-            for( int j = 0 ; j < n ; j++ ){
-                if( board[i][j] == 'X') continue;
-                // board[i][j] == 'O'
-                int index = i*n + j;
-                ids[index] = index;
-                if( i == 0 || j == 0 || i == m -1 || j == n - 1 ){
-                    // set index for the first time
-                    if( oEdge == - 1){
-                        oEdge = index;
-                    } else {
-                        // unite it with index 
-                        unite(oEdge,index);
-                    }
-                }
-                // to upper part
-                if( i > 0 && board[i-1][j] == 'O'){
-                    unite(index,index-n);
-                }
-                // to left part
-                if( j > 0 && board[i][j-1] == 'O'){
-                    unite(index,index-1);
-                }
-            }
+        if(board.length == 0 || board[0].length == 0) return;
+
+        // init, every char itself is an union
+        int m = board.length, n = board[0].length;
+        int size = m * n;
+        unionSet = new int[size];
+        hasEdgeO = new boolean[size];
+        for(int i = 0 ; i < size ; i++) {
+            unionSet[i] = i;
+        }    
+        for(int i = 0 ; i < size ; i++){
+            int x = i / n, y = i % n;
+            hasEdgeO[i] = (board[x][y] == 'O' && ( x == 0 || x == m - 1 || y == 0 || y == n-1));
         }
-        // generate new board
-        for (int i = 1; i < m - 1; i++) {
-            for (int j = 1; j < n - 1; j++) {
-                if (board[i][j] == 'X') {
-                    continue;
-                }
-                int index = i * n + j;
-                if (oEdge == -1 || !find(index, oEdge)) {
-                    board[i][j] = 'X';
-                }
-            }
+        
+        // union it + its upper char + its right char if they equals to each other
+        for(int i = 0 ; i < size ; i++){
+            int x = i / n, y = i % n, up = x - 1, right = y + 1;
+            if( up >= 0 && board[x][y] == board[up][y]) union(i,i-n);
+            if( right < n && board[x][y] == board[x][right]) union(i,i+1);
+        }
+        
+        //if it is an 'O' and its union doesn't has an 'edge O', the whole union should be setted as 'X'
+        for(int i = 0 ; i < size ; i++){
+            int x = i / n, y = i % n;
+            if( board[x][y] == 'O' && !hasEdgeO[findSet(i)]) board[x][y] = 'X'; 
         }
     }
-    private void unite(int a, int b){
-        int i = findRoot(a);
-        int j = findRoot(b);
-
-        if (sizes[i] < sizes[j]) {
-            ids[i] = j;
-            sizes[j] += sizes[i];
-        } else {
-            ids[j] = i;
-            sizes[i] += sizes[j];
-        }
+    
+    private void union(int x,int y){
+        int rootX = findSet(x);
+        int rootY = findSet(y);
+        // if there is an union has an 'edge O',the union after merge should be marked too
+        boolean flag = hasEdgeO[rootX] || hasEdgeO[rootY];
+        unionSet[rootX] = rootY;
+        hasEdgeO[rootY] = flag;
     }
-
-    private boolean find(int a, int b){
-        return findRoot(a) == findRoot(b);
-    }
-
-    private int findRoot(int i) {
-        while (i != ids[i]) {
-            // Path compression
-            ids[i] = ids[ids[i]];
-            i = ids[i];
-        }
-        return i;
+    
+    private int findSet(int x){
+        if(unionSet[x] == x) return x;
+        unionSet[x] = findSet(unionSet[x]);
+        return unionSet[x];
     }
 }

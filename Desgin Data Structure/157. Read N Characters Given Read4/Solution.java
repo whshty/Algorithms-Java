9598c1b9d37a0f423d106a54ee2b1d3837ce082d
teng <yangteng0813@gmail.com>
2017-10-23 21:06:40 -0400

    10.23

--- Desgin Data Structure/157. Read N Characters Given Read4/Solution.java
+++ Desgin Data Structure/157. Read N Characters Given Read4/Solution.java
@@ -5,12 +5,11 @@ public class Solution extends Reader4 {
      * @return    The number of characters read
      */
     public int read(char[] buf, int n) {
-        boolean isFinished = false;
         int index = 0;
         char[] bufForRead4 = new char[4];
-        while( !isFinished && index < n ){
+        while( index < n ){
             int totalToRead = read4(bufForRead4);
-            if( totalToRead < 4 ) isFinished = true; // totalToRead  == 4 -> false, continue to read
+            if( totalToRead == 0 ) break;
             totalToRead = Math.min(n-index,totalToRead);// if n - index = 2 but toRead is 3
             for( int i = 0 ; i < totalToRead ; i++ ){
                 buf[index++] = bufForRead4[i];

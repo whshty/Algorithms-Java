f3111b9d94b48dc4d7eb30bcf1485bf50e50ccb6
teng <yangteng0813@gmail.com>
2017-11-04 15:47:47 -0400

    11.4

--- Linear Structure/Pointer/Pointers/88. Merge Sorted Array/README.md
+++ Linear Structure/Pointer/Pointers/88. Merge Sorted Array/README.md
@@ -1,4 +1,4 @@
-## Two Pointers
+## 1.Two Pointers — Modify Input
 ```java
 public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
@@ -26,3 +26,41 @@ public class Solution {
     }
 }
 ```
+
+## 2.Two Pointers — Generate New Array
+```java
+public class Solution {
+    public int[] merge(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int i = m - 1;
+        int j = n - 1;
+        int index = n + m - 1;
+        int[] res = new int[m+n];
+
+        while ( i >= 0 && j >= 0 ){
+            if ( nums1[i] > nums2[j]){
+                res[index] = nums1[i];
+                i--;
+                index--;
+            } else {
+                res[index] = nums2[j];
+                j--;
+                index--;
+            }
+        }
+        while ( i >= 0 ){
+            res[index] = nums1[i];
+            index--;
+            i--;
+        }
+        while ( j >= 0 ){
+            res[index] = nums2[j];
+            index--;
+            j--;
+        }
+        return res;
+    }
+}
+```
+
--- Linear Structure/Pointer/Pointers/88. Merge Sorted Array/Solution.java
+++ Linear Structure/Pointer/Pointers/88. Merge Sorted Array/Solution.java
@@ -21,3 +21,50 @@ public class Solution {
         }
     }
 }
+
+// Generate new res
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] input1 = new int[]{5};
+        int[] input2 = new int[]{1,2,3,4};
+
+        Solution sol = new Solution();
+        int[] res = sol.merge(input1,input2);
+
+        for( int i : res ) System.out.print(i);
+
+    }
+    public int[] merge(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int i = m - 1;
+        int j = n - 1;
+        int index = n + m - 1;
+        int[] res = new int[m+n];
+
+        while ( i >= 0 && j >= 0 ){
+            if ( nums1[i] > nums2[j]){
+                res[index] = nums1[i];
+                i--;
+                index--;
+            } else {
+                res[index] = nums2[j];
+                j--;
+                index--;
+            }
+        }
+        while ( i >= 0 ){
+            res[index] = nums1[i];
+            index--;
+            i--;
+        }
+        while ( j >= 0 ){
+            res[index] = nums2[j];
+            index--;
+            j--;
+        }
+        return res;
+    }
+}

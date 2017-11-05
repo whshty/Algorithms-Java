6562cb420082a892b979240600f10a4bb084abf9
teng <yangteng0813@gmail.com>
2017-11-04 20:25:30 -0400

    11.4

--- BFS&DFS/1D/78. Subsets/Solution.java
+++ BFS&DFS/1D/78. Subsets/Solution.java
@@ -16,15 +16,15 @@ public class Solution {
 }
 
 // DFS
-public class Solution {
+class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<List<Integer>> res = new ArrayList<>();
         if(nums.length == 0) return res;
         helper(res,new ArrayList<>(),nums,0);
         return res;
     }
     private void helper(List<List<Integer>> res,List<Integer> list, int[] nums, int pos){
-        res.add(new ArrayList<Integer>(list));
+        res.add(new ArrayList<>(list));
         for(int i=pos;i<nums.length;i++){
             list.add(nums[i]);
             helper(res,list,nums,i+1);
@@ -32,7 +32,6 @@ public class Solution {
         }
     }
 }
-
 
 
 // Follow-Up:Multiplication

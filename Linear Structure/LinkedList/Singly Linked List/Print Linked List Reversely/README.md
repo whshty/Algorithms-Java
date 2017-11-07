174f1bceb238e8c0abc22312563b28d2b9aa6c79
teng <yangteng0813@gmail.com>
2017-11-06 21:11:37 -0500

    11.6

--- Linear Structure/LinkedList/Singly Linked List/Print Linked List Reversely/README.md
+++ Linear Structure/LinkedList/Singly Linked List/Print Linked List Reversely/README.md
@@ -1,9 +1,71 @@
+## 1.Recursion Print
 ```java
 class Solution {
     public void reversePrintInRecursion(ListNode head) {
         if (head == null) return;
         reversePrintInRecursion(head.next);
         System.out.print(head.val);
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+```
+
+## 2.Stack Print
+
+```java
+class Solution {
+    public void reversePrintByStack(ListNode head) {
+        Deque<ListNode> stack = new ArrayDeque<>();
+        while (head != null) {
+            stack.push(head);
+            head = head.next;
+        }
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+}
+```
+
+
+## 3.Divide and Conquer Print 
+
+```java
+class Solution {
+    public void printByDivide(ListNode head) {
+        ListNode cur = head;
+        int len = 0;
+        while (cur != null) {
+            cur = cur.next;
+            len++;
+        }
+        helper(head, len);
+    }
+
+    private void helper(ListNode head, int length) {
+        if (head == null) return;
+
+        if (length == 1) {
+            System.out.print(head.val);
+            return;
+        }
+
+        int count = 0;
+        ListNode cur = head;
+        while (count < length / 2) {
+            cur = cur.next;
+            count++;
+        }
+        helper(cur, length - length / 2);
+        helper(head, length / 2);
     }
 }
 
--- Linear Structure/LinkedList/Singly Linked List/Print Linked List Reversely/Solution.java
+++ Linear Structure/LinkedList/Singly Linked List/Print Linked List Reversely/Solution.java
@@ -1,3 +1,6 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 class Solution {
     public static void main(String[] args) throws java.lang.Exception {
         ListNode head = new ListNode(1);
@@ -11,12 +14,56 @@ class Solution {
 
         Solution sol = new Solution();
         sol.reversePrintInRecursion(head);
+        System.out.println();
+        sol.reversePrintInRecursion(head);
+        System.out.println();
+        sol.printByDivide(head);
+
     }
 
     public void reversePrintInRecursion(ListNode head) {
         if (head == null) return;
         reversePrintInRecursion(head.next);
         System.out.print(head.val);
+    }
+
+    public void reversePrintByStack(ListNode head) {
+        Deque<ListNode> stack = new ArrayDeque<>();
+        while (head != null) {
+            stack.push(head);
+            head = head.next;
+        }
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+
+    public void printByDivide(ListNode head) {
+        ListNode cur = head;
+        int len = 0;
+        while (cur != null) {
+            cur = cur.next;
+            len++;
+        }
+        helper(head, len);
+    }
+
+    private void helper(ListNode head, int length) {
+        if (head == null) return;
+
+        if (length == 1) {
+            System.out.print(head.val);
+            return;
+        }
+
+        int count = 0;
+        ListNode cur = head;
+        while (count < length / 2) {
+            cur = cur.next;
+            count++;
+        }
+        helper(cur, length - length / 2);
+        helper(head, length / 2);
     }
 }
 

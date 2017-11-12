19f2961bcd72341addf5b9f98b9dafb3549963e6
teng <yangteng0813@gmail.com>
2017-11-12 18:24:28 -0500

    11.12

--- Linear Structure/Hash/350. Intersection of Two Arrays II/README.md
+++ Linear Structure/Hash/350. Intersection of Two Arrays II/README.md
@@ -24,7 +24,7 @@ class Solution {
 }
 ```
 
-## 2.Follow Up (Sorted Input) - Two Pointer
+## 2.Follow Up (Two Sorted Input) - Two Pointer
 
 ```java
 public class Solution {
@@ -48,6 +48,54 @@ public class Solution {
             res[index] = list.get(index);
         }
         return res;
+    }
+}
+```
+
+## 3.Binary Search 
+* Two array are sorted
+* A is much longer than B
+
+```java
+public class Solution {
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        List<Integer> list = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        // the size of num1 is much more bigger than nums2 and nums1 is sparse
+        // nums1 = [1,1,1,1,1,1,1,2,2,2,3,4,5,6,7,7,7,8,9]
+        // nums2 = [6,7]
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i = binarySearch(nums1,nums2[j],i,nums1.length-1);
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] res = new int[list.size()];
+        int index = 0;
+        for (Integer num : list) {
+            res[index++] = num;
+        }
+        return res;
+    }
+
+    public int binarySearch(int[] nums, int target, int start, int end) {
+        while ( start < end ) {
+            int mid = ( start + end ) >>> 1;
+            if(nums[mid] < target){
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
     }
 }
 ```
--- Linear Structure/Hash/350. Intersection of Two Arrays II/Solution.java
+++ Linear Structure/Hash/350. Intersection of Two Arrays II/Solution.java
@@ -45,3 +45,58 @@ public class Solution {
         return res;
     }
 }
+
+
+// Binary Search on two sorted array
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] intput1 = new int[]{1,1,1,1,1,1,1,2,2,2,3,4,5,6,7,7,7,8,9};
+        int[] intput2 = new int[]{1,1,7,7};
+
+        Solution sol = new Solution();
+
+        int[] res = sol.intersection(intput1,intput2);
+        for( int i : res ) System.out.println(i);
+
+    }
+    public int[] intersection(int[] nums1, int[] nums2) {
+        List<Integer> list = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        // the size of num1 is much more bigger than nums2 and nums1 is sparse
+        // nums1 = [1,1,1,1,1,1,1,2,2,2,3,4,5,6,7,7,7,8,9]
+        // nums2 = [6,7]
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i = binarySearch(nums1,nums2[j],i,nums1.length-1);
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] res = new int[list.size()];
+        int index = 0;
+        for (Integer num : list) {
+            res[index++] = num;
+        }
+        return res;
+    }
+
+    public int binarySearch(int[] nums, int target, int start, int end) {
+        while ( start < end ) {
+            int mid = ( start + end ) >>> 1;
+            if(nums[mid] < target){
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+}

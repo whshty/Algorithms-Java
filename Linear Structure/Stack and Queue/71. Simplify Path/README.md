62e4f0a0efee0f3e1ce3acd73fd2a3b71a78e2c2
teng <yangteng0813@gmail.com>
2017-11-12 22:03:04 -0500

    11.12

--- Linear Structure/Stack and Queue/71. Simplify Path/README.md
+++ Linear Structure/Stack and Queue/71. Simplify Path/README.md
@@ -1,4 +1,4 @@
-## Stack
+## 1.Stack
 ```java
 public class Solution {
     public String simplifyPath(String path) {
@@ -21,6 +21,33 @@ public class Solution {
             sb.append("/").append(stack.pollLast());
         }
         return sb.toString();
+    }
+}
+```
+
+## 2.Edit String Array
+
+```java
+public class Solution {
+    public String simplifyPath(String path) {
+        if (path == null || path.charAt(0) != '/') return null;  
+        String[] splits = path.split("/");  
+   
+        int count = 0;  
+        for (String str : splits) {  
+            if (str.equals("..") && count > 0) --count;  
+            else if (!str.equals(".") && !str.equals("..") && !str.equals("")) {  
+                splits[count++] = str;  
+            }  
+        }  
+   
+        // print new path  
+        if (count == 0) return "/";  
+        StringBuilder sb = new StringBuilder();  
+        for (int i = 0 ; i < count ; ++i) {  
+            sb.append("/" + splits[i]);  
+        }  
+        return sb.toString();  
     }
 }
 ```
--- Linear Structure/Stack and Queue/71. Simplify Path/Solution.java
+++ Linear Structure/Stack and Queue/71. Simplify Path/Solution.java
@@ -1,3 +1,4 @@
+//stack
 public class Solution {
     public String simplifyPath(String path) {
         if( path == null || path.length() == 0 ) return "";
@@ -19,5 +20,29 @@ public class Solution {
             sb.append("/").append(stack.pollLast());
         }
         return sb.toString();
+    }
+}
+
+// Edit array
+public class Solution {
+    public String simplifyPath(String path) {
+        if (path == null || path.charAt(0) != '/') return null;  
+        String[] splits = path.split("/");  
+   
+        int count = 0;  
+        for (String str : splits) {  
+            if (str.equals("..") && count > 0) --count;  
+            else if (!str.equals(".") && !str.equals("..") && !str.equals("")) {  
+                splits[count++] = str;  
+            }  
+        }  
+   
+        // print new path  
+        if (count == 0) return "/";  
+        StringBuilder sb = new StringBuilder();  
+        for (int i = 0 ; i < count ; ++i) {  
+            sb.append("/" + splits[i]);  
+        }  
+        return sb.toString();  
     }
 }

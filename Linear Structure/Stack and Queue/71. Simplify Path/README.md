552919693bf99bc1c69a27df8ff0afff38b8a617
teng <yangteng0813@gmail.com>
2017-11-12 22:06:26 -0500

    11.12

--- Linear Structure/Stack and Queue/71. Simplify Path/README.md
+++ Linear Structure/Stack and Queue/71. Simplify Path/README.md
@@ -29,25 +29,25 @@ public class Solution {
 
 ```java
 public class Solution {
-    public String simplifyPath(String path) {
-        if (path == null || path.charAt(0) != '/') return null;  
-        String[] splits = path.split("/");  
-   
-        int count = 0;  
-        for (String str : splits) {  
-            if (str.equals("..") && count > 0) --count;  
-            else if (!str.equals(".") && !str.equals("..") && !str.equals("")) {  
-                splits[count++] = str;  
-            }  
-        }  
-   
-        // print new path  
-        if (count == 0) return "/";  
-        StringBuilder sb = new StringBuilder();  
-        for (int i = 0 ; i < count ; ++i) {  
-            sb.append("/" + splits[i]);  
-        }  
-        return sb.toString();  
+    public String simplifyPath(String input) {
+        if (input == null || input.charAt(0) != '/') return null;
+        String[] splits = input.split("/");
+
+        int count = 0;
+        for (String str : splits) {
+            if (str.equals("..") && count > 0) --count;
+            else if (!str.equals(".") && !str.equals("..") && !str.equals("")) {
+                splits[count++] = str;
+            }
+        }
+
+        // print new path
+        if (count == 0) return "/";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0 ; i < count ; ++i) {
+            sb.append("/" + splits[i]);
+        }
+        return sb.toString();
     }
 }
 ```
--- Linear Structure/Stack and Queue/71. Simplify Path/Solution.java
+++ Linear Structure/Stack and Queue/71. Simplify Path/Solution.java
@@ -1,19 +1,19 @@
 //stack
 public class Solution {
-    public String simplifyPath(String path) {
-        if( path == null || path.length() == 0 ) return "";
+    public String simplifyPath(String input) {
+        if( input == null || input.length() == 0 ) return "";
         Deque<String> stack = new ArrayDeque<>();
-        
+
         //"" for corner case of "/home//foo/"
-        for(String dir : path.split("/")){
+        for(String dir : input.split("/")){
             if ( dir.equals(".") || dir.equals("") ) continue;
             else if( dir.equals("..")) {
                 if( !stack.isEmpty()) stack.pop();
             }
             else stack.push(dir);
         }
-        
-  
+
+
         StringBuilder sb = new StringBuilder();
         if( stack.isEmpty() ) sb.append("/");
         while( !stack.isEmpty()){
@@ -25,24 +25,24 @@ public class Solution {
 
 // Edit array
 public class Solution {
-    public String simplifyPath(String path) {
-        if (path == null || path.charAt(0) != '/') return null;  
-        String[] splits = path.split("/");  
-   
-        int count = 0;  
-        for (String str : splits) {  
-            if (str.equals("..") && count > 0) --count;  
-            else if (!str.equals(".") && !str.equals("..") && !str.equals("")) {  
-                splits[count++] = str;  
-            }  
-        }  
-   
-        // print new path  
-        if (count == 0) return "/";  
-        StringBuilder sb = new StringBuilder();  
-        for (int i = 0 ; i < count ; ++i) {  
-            sb.append("/" + splits[i]);  
-        }  
-        return sb.toString();  
+    public String simplifyPath(String input) {
+        if (input == null || input.charAt(0) != '/') return null;
+        String[] splits = input.split("/");
+
+        int count = 0;
+        for (String str : splits) {
+            if (str.equals("..") && count > 0) --count;
+            else if (!str.equals(".") && !str.equals("..") && !str.equals("")) {
+                splits[count++] = str;
+            }
+        }
+
+        // print new path
+        if (count == 0) return "/";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0 ; i < count ; ++i) {
+            sb.append("/" + splits[i]);
+        }
+        return sb.toString();
     }
 }

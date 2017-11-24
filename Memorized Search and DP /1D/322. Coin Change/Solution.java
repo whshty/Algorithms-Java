a7289847ffd3a52fcc84d2bdd717be731e4cd009
teng <yangteng0813@gmail.com>
2017-11-24 14:12:07 -0500

    11.24

---
+++ Memorized Search and DP /1D/322. Coin Change/Solution.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if( amount < 1 ) return 0;
+        return dfs(coins,amount);
+    }
+    
+    public int dfs(int[] coins, int value){
+        if( value < 0 ) return -1;
+        if( value == 0) return 0;
+        
+        int res = 0;
+        int min = Integer.MAX_VALUE;
+        for( int i : coins ){
+            res = dfs(coins,value-i);
+            if( res >= 0 && res < min ) min = 1 + res;
+        }
+        if( min == Integer.MAX_VALUE ) return -1;
+        else return min;
+    }
+}
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if( amount < 1 ) return 0;
+        return dfs(coins,amount, new HashMap<Integer,Integer>());
+    }
+    
+    public int dfs(int[] coins, int value, Map<Integer,Integer> map){
+        if( value < 0 ) return -1;
+        if( value == 0) return 0;
+        if(map.containsKey(value)) return map.get(value);
+        
+        int res = 0;
+        int min = Integer.MAX_VALUE;
+        for( int i : coins ){
+            res = dfs(coins,value-i,map);
+            if( res >= 0 && res < min ) min = 1 + res;
+        }
+        if( min == Integer.MAX_VALUE ) return -1;
+        map.put(value,min);
+        return min;
+    }
+}

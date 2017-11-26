614928ef5e1e91ecb79c9b98a4d001830d3c2ea3
teng <yangteng0813@gmail.com>
2017-11-25 20:09:33 -0500

    11.25

--- Memorized Search and DP /1D/322. Coin Change/Solution.java
+++ Memorized Search and DP /1D/322. Coin Change/Solution.java
@@ -41,3 +41,20 @@ class Solution {
         return min;
     }
 }
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        dp[0] = 0;
+        for( int i = 1 ; i <= amount ; i++ ){
+            dp[i] = Integer.MAX_VALUE;
+            for( int j : coins ){
+                if( i >= j && dp[i-j] != Integer.MAX_VALUE){
+                    dp[i] = Math.min(dp[i],dp[i-j] +1 );
+                }
+            }
+        }
+        if (dp[amount] == Integer.MAX_VALUE) return -1;
+        return dp[amount];
+    }
+}

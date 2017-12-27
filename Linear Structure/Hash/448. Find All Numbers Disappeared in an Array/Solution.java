3604e226bcb4aa344cf56f95917098897c1305b4
teng <yangteng0813@gmail.com>
2017-12-27 16:09:03 -0500

    12.27

--- Linear Structure/Hash/448. Find All Numbers Disappeared in an Array/Solution.java
+++ Linear Structure/Hash/448. Find All Numbers Disappeared in an Array/Solution.java
@@ -11,3 +11,18 @@ class Solution {
         return res;
     }
 }
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for ( int i = 0 ; i < nums.length ; i++ ){
+            int index = Math.abs(nums[i]) - 1;
+            if ( nums[index] > 0 ) nums[index] = -nums[index];
+        }
+        
+        for( int i = 0 ; i < nums.length ; i++ ){
+            if( nums[i] > 0 ) res.add(i+1);
+        }
+        return res;
+    }
+}

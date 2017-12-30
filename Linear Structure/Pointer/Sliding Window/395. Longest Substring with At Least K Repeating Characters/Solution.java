b3fd5a654673f18222a17a491ea3b4f4bfc6c121
teng <yangteng0813@gmail.com>
2017-12-30 16:00:12 -0500

    12.30

--- Linear Structure/Pointer/Sliding Window/395. Longest Substring with At Least K Repeating Characters/Solution.java
+++ Linear Structure/Pointer/Sliding Window/395. Longest Substring with At Least K Repeating Characters/Solution.java
@@ -1,4 +1,4 @@
-class Solution {
+public class Solution {
     public int longestSubstring(String s, int count) {
         char[] str = s.toCharArray();
         int[] freq = new int[26];
@@ -6,26 +6,26 @@ class Solution {
 
         for (int i = 1; i <= 26; i++) {
             Arrays.fill(freq, 0);
-            int j = 0;
-            int k = 0;
+            int left = 0;
+            int right = 0;
             int uniqueCount = 0;
             int noLessThanK = 0;
-            int index = 0;
-            while (index < str.length) {
+            while (right < str.length) {
                 if (uniqueCount <= i) {
-                    int ch = str[j] - 'a';
+                    int ch = str[right] - 'a';
                     if (freq[ch] == 0) uniqueCount++;
                     freq[ch]++;
                     if (freq[ch] == count) noLessThanK++;
-                    j++;
-                } else {
-                    int ch = str[k] - 'a';
+                    right++;
+                }
+                else {
+                    int ch = str[left] - 'a';
                     if (freq[ch] == count) noLessThanK--;
                     freq[ch]--;
                     if (freq[ch] == 0) uniqueCount--;
-                    k++;
+                    left++;
                 }
-                if (uniqueCount == i && uniqueCount == noLessThanK) res = Math.max(j - k, res);
+                if (uniqueCount == i && uniqueCount == noLessThanK) res = Math.max(right - left, res);
             }
         }
         return res;

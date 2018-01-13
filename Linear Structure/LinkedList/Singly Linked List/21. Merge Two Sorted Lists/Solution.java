b16bd0d9bef0ed137b0ce977a015be162f5b790b
teng <yangteng0813@gmail.com>
2018-01-12 23:08:50 -0500

    1.12

--- Linear Structure/LinkedList/Singly Linked List/21. Merge Two Sorted Lists/Solution.java
+++ Linear Structure/LinkedList/Singly Linked List/21. Merge Two Sorted Lists/Solution.java
@@ -1,24 +1,20 @@
-public class Solution {
+class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode head = dummy;
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
         while( l1 != null && l2 != null ){
             if( l1.val < l2.val ){
-                head.next = l1;
+                cur.next = l1;
                 l1 = l1.next;
-            } 
-            else {
-                head.next = l2;
+            } else {
+                cur.next =l2;
                 l2 = l2.next;
             }
-            head = head.next;
+            cur = cur.next;
         }
-        if( l1 == null ){
-            head.next = l2;
-        } 
-        else if ( l2 == null ){
-            head.next = l1;
-        }
+        if( l1 == null ) cur.next = l2;
+        else if ( l2 == null ) cur.next = l1;
+
         return dummy.next;
     }
 }

d1d7936105690d1e2891efb288b82226e6a3394c
teng <yangteng0813@gmail.com>
2018-01-16 14:03:58 -0500

    1.16

---
+++ Linear Structure/Scan and Search/728. Self Dividing Numbers/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> res = new ArrayList<>();
+        for( int i = left ; i <= right ; i++ ){
+            int cur = i;
+            while( cur > 0 ){
+                if( ( cur % 10 == 0 ) || ( i % ( cur % 10 ) != 0 )) break;
+                cur /= 10;
+            }
+            if( cur == 0 ) res.add(i);
+        }
+        return res;
+
+    }
+}

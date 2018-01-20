8ffa3ba67be84fc6d6ca1693bfafb394a7e30c1a
teng <yangteng0813@gmail.com>
2018-01-20 18:05:47 -0500

    1.20

--- Binary Search/658. Find K Closest Elements/Solution.java
+++ Binary Search/658. Find K Closest Elements/Solution.java
@@ -1,18 +1,7 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int start = 0, end = arr.length-1;
-        while( start < end ) {
-            int mid = (start + end) >>> 1;
-            if(arr[mid] == x) {
-                end = mid;
-                break;
-            }
-            else if( arr[mid] > x) end = mid-1;
-            else start = mid + 1;
-        }
-
-        start = end;
-        end++;
+        int start = binarySearch(arr,x);
+        int end = start + 1 ;
         while( k > 0 ) {
             if( end >= arr.length || ( start >= 0 && x - arr[start] <= arr[end] - x) )  start--;
             else end++;
@@ -24,5 +13,15 @@ class Solution {
             res.add(arr[i]);
         }
         return res;
+    }
+    private int binarySearch(int[] arr , int target ){
+        int start = 0, end = arr.length - 1;
+        while( start < end ){
+            int mid = ( start + end ) >>> 1;
+            if( arr[mid] == target ) return mid;
+            else if( arr[mid] > target ) end = mid - 1;
+            else start = mid + 1;
+        }
+        return start;
     }
 }

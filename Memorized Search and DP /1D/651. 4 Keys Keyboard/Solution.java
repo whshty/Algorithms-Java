812bda55d1233051037c4898f1bf8faceeae3541
teng <yangteng0813@gmail.com>
2018-01-25 21:47:16 -0500

    1.125

---
+++ Memorized Search and DP /1D/651. 4 Keys Keyboard/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxA(int N) {
+        int[] dp = new int[N+1];
+        for( int i = 0 ; i <= N ; i++ ){
+            dp[i] = i;
+            for( int j = 3 ; j < i ; j++ ){
+                dp[i] = Math.max(dp[i],dp[i-j]*(j-1));
+            }
+        }
+        return dp[N];
+    }
+}

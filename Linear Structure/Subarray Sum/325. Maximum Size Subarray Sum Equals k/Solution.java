03f683b21f1879e3c6f67c785060cb2ed63e564e
teng <yangteng0813@gmail.com>
2018-02-22 22:16:36 -0800

    2.22

--- Linear Structure/Subarray Sum/325. Maximum Size Subarray Sum Equals k/Solution.java
+++ Linear Structure/Subarray Sum/325. Maximum Size Subarray Sum Equals k/Solution.java
@@ -1,15 +1,16 @@
-public class Solution {
-    public int maxSubArrayLen(int[] nums, int k) {
-        if( nums == null || nums.length == 0) return 0;
-        int sum = 0;
+class Solution {
+    public int maxSubArrayLen(int[] nums, int target) {
         int res = 0;
+        if( nums == null || nums.length == 0 ) return res;
         Map<Integer,Integer> map = new HashMap<>();
-        map.put(0,-1); 
-        for( int i = 0; i < nums.length ; i++){
+        map.put(0,-1);
+        int sum = 0;
+        
+        for( int i = 0 ; i < nums.length ; i++ ){
             sum += nums[i];
             map.putIfAbsent(sum,i);
-            if( map.containsKey(sum - k) ){
-                res = Math.max( res, i - map.get(sum-k) );
+            if(map.containsKey(sum-target)){
+                res = Math.max(res,i-map.get(sum-target));
             }
         }
         return res;

7604a052aed6406d5de1a6d9173a1a074f47c439
teng <yangteng0813@gmail.com>
2018-02-25 11:51:48 -0800

    2.25

--- Heap/692. Top K Frequent Words/Solution.java
+++ Heap/692. Top K Frequent Words/Solution.java
@@ -21,3 +21,23 @@ class Solution {
         return res;
     }
 }
+
+// maxHeap
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        List<String> res = new LinkedList<>();
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            map.put(words[i],map.getOrDefault(words[i],0)+1);
+        }
+
+        PriorityQueue<Map.Entry<String, Integer>> maxHeap = new PriorityQueue<>(
+                (a, b) -> a.getValue() == b.getValue() ? a.getKey().compareTo(b.getKey()) : b.getValue() - a.getValue()
+        );
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) maxHeap.offer(entry);
+        for( int i = 0 ; i < k ; i++ ) res.add(maxHeap.poll().getKey());
+
+        return res;
+    }
+}

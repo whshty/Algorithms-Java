ec6dc114232ab262541dc9b245a41dbefabd950f
teng <yangteng0813@gmail.com>
2018-03-03 21:58:34 -0800

    3.3

--- BFS&DFS/1D/737. Sentence Similarity II/Solution.java
+++ BFS&DFS/1D/737. Sentence Similarity II/Solution.java
@@ -5,14 +5,14 @@ class Solution {
         
         for( String[] pair : pairs ){
             String str1 = dfs(map,pair[0]) , str2 = dfs(map,pair[1]);
-            if( !str1.equals(str2)) map.put(str1,str2);
+            if(!str1.equals(str2)) map.put(str1,str2);
         }
         for( int i = 0 ; i < words1.length ; i++ ){
-            if( !words1[i].equals(words2[i]) && !dfs(map,words1[i]).equals(dfs(map,words2[i]))) return false;
+            if(!words1[i].equals(words2[i]) && !dfs(map,words1[i]).equals(dfs(map,words2[i]))) return false;
         }
         return true;
-        
     }
+    
     private String dfs(Map<String,String> map , String str){
         if( !map.containsKey(str) ) map.put(str,str);
         return str.equals(map.get(str)) ? str : dfs(map,map.get(str));

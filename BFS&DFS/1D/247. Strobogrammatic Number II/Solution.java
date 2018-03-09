38c389d14445cbbdf4500a818313f4c75dbd587d
teng <yangteng0813@gmail.com>
2018-03-08 23:09:43 -0800

    3.8

--- BFS&DFS/1D/247. Strobogrammatic Number II/Solution.java
+++ BFS&DFS/1D/247. Strobogrammatic Number II/Solution.java
@@ -5,41 +5,41 @@ class Solution {
         helper(res,new char[n],0,n-1);
         return res;
     }
-    public void helper(List<String> res,char[] ch ,int left , int right){
+    public void helper(List<String> res,char[] chs ,int left , int right){
         if( left > right ) {
-            if( !res.contains(new String(ch))){
-                res.add(new String(ch));     
+            if( !res.contains(new String(chs))){
+                res.add(new String(chs));     
             }
             return;
         }
         if( left == right ){
-            ch[left] = '0';
-            res.add(new String(ch));
-            ch[left] = '1';
-            res.add(new String(ch));
-            ch[left] = '8';
-            res.add(new String(ch));
+            chs[left] = '0';
+            res.add(new String(chs));
+            chs[left] = '1';
+            res.add(new String(chs));
+            chs[left] = '8';
+            res.add(new String(chs));
             return;
         }
         if( left != 0 ){
-            ch[left] = '0';
-            ch[right] = '0';
-            helper(res,ch,left+1,right-1);
+            chs[left] = '0';
+            chs[right] = '0';
+            helper(res,chs,left+1,right-1);
         }
-        ch[left] = '1';
-        ch[right] = '1';
-        helper(res,ch,left+1,right-1);
+        chs[left] = '1';
+        chs[right] = '1';
+        helper(res,chs,left+1,right-1);
         
-        ch[left] = '8';
-        ch[right] = '8';
-        helper(res,ch,left+1,right-1);
+        chs[left] = '8';
+        chs[right] = '8';
+        helper(res,chs,left+1,right-1);
     
-        ch[left] = '6';
-        ch[right] = '9';
-        helper(res,ch,left+1,right-1);
+        chs[left] = '6';
+        chs[right] = '9';
+        helper(res,chs,left+1,right-1);
     
-        ch[left] = '9';
-        ch[right] = '6';
-        helper(res,ch,left+1,right-1);
+        chs[left] = '9';
+        chs[right] = '6';
+        helper(res,chs,left+1,right-1);
     }
 }

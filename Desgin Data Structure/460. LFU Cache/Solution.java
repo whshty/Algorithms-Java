acaa2f2a3f1b2b59ba0ba7d00ed6141bdc53728a
Teng Yang <yangteng0813@gmail.com>
2018-03-20 22:06:13 -0700

    3.20

---
+++ Desgin Data Structure/460. LFU Cache/Solution.java
@@ -0,0 +1,46 @@
+class LFUCache {
+    Map<Integer,Integer> valMap;
+    Map<Integer,Integer> freqMap;
+    Map<Integer,LinkedHashSet<Integer>> listMap;
+    int size;
+    int min = -1;
+
+    public LFUCache(int size) {
+        this.size = size;
+        valMap = new HashMap<>();
+        freqMap = new HashMap<>();
+        listMap = new HashMap<>();
+        listMap.put(1,new LinkedHashSet<>());
+    }
+    
+    public int get(int key) {
+        if(!valMap.containsKey(key)) return -1;
+        int freq = freqMap.get(key);
+        freqMap.put(key,freq+1);
+        listMap.get(freq).remove(key);
+        if( freq == min && listMap.get(freq).size() == 0) min++;
+        if( !listMap.containsKey(freq+1)){
+            listMap.put(freq+1,new LinkedHashSet<>());
+        }
+        listMap.get(freq+1).add(key);
+        return valMap.get(key);
+    }
+    
+    public void put(int key, int value) {
+        if( size <= 0 ) return;
+        if(valMap.containsKey(key)){
+            valMap.put(key,value);
+            get(key);
+            return;
+        }
+        if(valMap.size() >= size){
+            int evit = listMap.get(min).iterator().next();
+            listMap.get(min).remove(evit);
+            valMap.remove(evit);
+        }
+        valMap.put(key,value);
+        freqMap.put(key,1);
+        min = 1;
+        listMap.get(1).add(key);
+    }
+}

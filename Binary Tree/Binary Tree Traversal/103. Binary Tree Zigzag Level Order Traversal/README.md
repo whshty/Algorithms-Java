5dc8d4d50d6d8ea089cbaeeaee23adf55588f293
teng <yangteng0813@gmail.com>
2018-03-31 16:16:17 -0700

    3.31

--- Binary Tree/Binary Tree Traversal/103. Binary Tree Zigzag Level Order Traversal/README.md
+++ Binary Tree/Binary Tree Traversal/103. Binary Tree Zigzag Level Order Traversal/README.md
@@ -1,8 +1,44 @@
-# 103. Binary Tree Zigzag Level Order Traversal
-## Idea
-* Level order traversal with two directions
-* Use deque and a direction flag
+## Deque
 
-## Time & Space
-* O(n)
-* O(n)
+```java
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if( root == null ) return res;
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        boolean leftToRight = true;
+        deque.push(root);
+        
+        while(!deque.isEmpty()){
+            int size = deque.size();
+            List<Integer> list = new ArrayList<>();
+            if(leftToRight){
+                addTreeNodeFromLeftToRight(deque,list,size);
+            } else{
+                addTreeNodeFromRightToLeft(deque,list,size);
+            }
+            res.add(list);
+            leftToRight = leftToRight ? false : true;
+        }
+        return res;
+    }
+    
+    private void addTreeNodeFromLeftToRight(Deque<TreeNode> deque, List<Integer> list, int size ){
+        for (int i = 0 ; i < size ; i++){
+            TreeNode node = deque.pollFirst();
+            list.add(node.val);  
+            if (node.left != null ) deque.addLast(node.left);
+            if (node.right != null) deque.addLast(node.right);
+        }
+    }
+    
+    private void addTreeNodeFromRightToLeft(Deque<TreeNode> deque, List<Integer> list, int size){
+        for(int i = 0 ; i < size ; i++){
+            TreeNode node = deque.pollLast();
+            list.add(node.val);
+            if (node.right != null ) deque.addFirst(node.right);
+            if (node.left != null ) deque.addFirst(node.left);
+        }
+    }
+}
+```
--- Binary Tree/Binary Tree Traversal/103. Binary Tree Zigzag Level Order Traversal/Solution.java
+++ Binary Tree/Binary Tree Traversal/103. Binary Tree Zigzag Level Order Traversal/Solution.java
@@ -1,41 +1,40 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-public class Solution {
+class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         if( root == null ) return res;
-        Deque<TreeNode> deque = new LinkedList<TreeNode>();
+        Deque<TreeNode> deque = new ArrayDeque<>();
         boolean leftToRight = true;
-        deque.addFirst(root);
+        deque.push(root);
         
         while(!deque.isEmpty()){
             int size = deque.size();
             List<Integer> list = new ArrayList<>();
             if(leftToRight){
-                for(int i = 0 ; i < size ; i++){
-                    TreeNode node = deque.pollFirst();
-                    list.add(node.val);  
-                    if(node.left != null ) deque.addLast(node.left);
-                    if(node.right != null) deque.addLast(node.right);
-                }
+                addTreeNodeFromLeftToRight(deque,list,size);
             } else{
-                for(int i = 0 ; i < size ; i++){
-                    TreeNode node = deque.pollLast();
-                    list.add(node.val);
-                    if(node.right != null ) deque.addFirst(node.right);
-                    if(node.left != null ) deque.addFirst(node.left);
-                }
+                addTreeNodeFromRightToLeft(deque,list,size);
             }
             res.add(list);
             leftToRight = leftToRight ? false : true;
         }
         return res;
+    }
+    
+    private void addTreeNodeFromLeftToRight(Deque<TreeNode> deque, List<Integer> list, int size ){
+        for (int i = 0 ; i < size ; i++){
+            TreeNode node = deque.pollFirst();
+            list.add(node.val);  
+            if (node.left != null ) deque.addLast(node.left);
+            if (node.right != null) deque.addLast(node.right);
+        }
+    }
+    
+    private void addTreeNodeFromRightToLeft(Deque<TreeNode> deque, List<Integer> list, int size){
+        for(int i = 0 ; i < size ; i++){
+            TreeNode node = deque.pollLast();
+            list.add(node.val);
+            if (node.right != null ) deque.addFirst(node.right);
+            if (node.left != null ) deque.addFirst(node.left);
+        }
     }
 }

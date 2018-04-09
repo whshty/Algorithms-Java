a0e7525bb4ea00b32b87643ad550f4050ffec4a2
teng <yangteng0813@gmail.com>
2018-04-08 20:02:45 -0700

    4.8

--- Binary Tree/Top-Down/257. Binary Tree Paths/Solution.java
+++ Binary Tree/Top-Down/257. Binary Tree Paths/Solution.java
@@ -1,28 +1,28 @@
-public class Solution {
+class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> res = new ArrayList<>();
-        if(root == null ) return res;
+        if( root == null ) return res;
         helper(root,res,new StringBuilder());
         return res;
+        
     }
     private void helper(TreeNode node, List<String> res, StringBuilder sb){
         int len = sb.length();
         sb.append(node.val);
-        if( node.left == null && node.right == null ) {
+        if( node.left == null && node.right == null ){
             res.add(sb.toString());
-        } else{
-            sb.append("->");            
-            if( node.left != null ){
+        } else { 
+            sb.append("->");
+            if (node.left != null ){
                 helper(node.left,res,sb);
             }
-            if( node.right != null ){
+            if(node.right != null ){
                 helper(node.right,res,sb);
             }
         }
         sb.setLength(len);
     }
 }
-
 public class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> res = new ArrayList<>();

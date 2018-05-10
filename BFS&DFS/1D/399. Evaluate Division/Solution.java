1720f03b0b4f247ee31b359f6f213f9f98231062
teng <yangteng0813@gmail.com>
2018-05-10 10:32:55 -0400

    5.10

--- BFS&DFS/1D/399. Evaluate Division/Solution.java
+++ BFS&DFS/1D/399. Evaluate Division/Solution.java
@@ -1,43 +1,43 @@
 public class Solution {
     public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
-        Map<String, ArrayList<String>> pairs = new HashMap<>();
-        Map<String, ArrayList<Double>> valuesPair = new HashMap<>();
+        Map<String, ArrayList<String>> valueToValue = new HashMap<>();
+        Map<String, ArrayList<Double>> valueToResult = new HashMap<>();
         for (int i = 0; i < equations.length; i++) {
             String[] equation = equations[i];
-            
-            pairs.computeIfAbsent(equation[0], list -> new ArrayList<>()).add(equation[1]);
-            pairs.computeIfAbsent(equation[1], list -> new ArrayList<>()).add(equation[0]);
-            valuesPair.computeIfAbsent(equation[0], list -> new ArrayList<>()).add(values[i]);
-            valuesPair.computeIfAbsent(equation[1], list -> new ArrayList<>()).add(1/values[i]);
+
+            valueToValue.computeIfAbsent(equation[0], list -> new ArrayList<>()).add(equation[1]);
+            valueToValue.computeIfAbsent(equation[1], list -> new ArrayList<>()).add(equation[0]);
+            valueToResult.computeIfAbsent(equation[0], list -> new ArrayList<>()).add(values[i]);
+            valueToResult.computeIfAbsent(equation[1], list -> new ArrayList<>()).add(1/values[i]);
         }
-        
+
         double[] res = new double[queries.length];
         for (int i = 0; i < queries.length; i++) {
             String[] query = queries[i];
-            res[i] = dfs(query[0], query[1], pairs, valuesPair, new HashSet<>(), 1.0);
+            res[i] = dfs(query[0], query[1], valueToValue, valueToResult, new HashSet<>(), 1.0);
             if (res[i] == 0.0) res[i] = -1.0;
         }
         return res;
     }
-    
+
     private double dfs(
-        String start, 
-        String end, 
-        Map<String, ArrayList<String>> pairs, 
-        Map<String, ArrayList<Double>> values, 
-        HashSet<String> set, 
-        double value) {
-        
+            String start,
+            String end,
+            Map<String, ArrayList<String>> valueToValue,
+            Map<String, ArrayList<Double>> valueToResult,
+            HashSet<String> set,
+            double value) {
+
         if (set.contains(start)) return 0.0;
-        if (!pairs.containsKey(start)) return 0.0;
+        if (!valueToValue.containsKey(start)) return 0.0;
         if (start.equals(end)) return value;
         set.add(start);
-        
-        List<String> strList = pairs.get(start);
-        List<Double> valueList = values.get(start);
+
+        List<String> strList = valueToValue.get(start);
+        List<Double> valueList = valueToResult.get(start);
         double res = 0.0;
         for (int i = 0; i < strList.size(); i++) {
-            res = dfs(strList.get(i), end, pairs, values, set, value*valueList.get(i));
+            res = dfs(strList.get(i), end, valueToValue, valueToResult, set, value*valueList.get(i));
             if (res != 0.0) break;
         }
         set.remove(start);

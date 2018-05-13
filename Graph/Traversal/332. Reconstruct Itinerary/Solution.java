d5f87c451053d9390ac1c9775a04eb036aed5c11
teng <yangteng0813@gmail.com>
2018-05-13 16:08:12 -0400

    5.13

--- Graph/Traversal/332. Reconstruct Itinerary/Solution.java
+++ Graph/Traversal/332. Reconstruct Itinerary/Solution.java
@@ -1,3 +1,31 @@
+// Iteration
+class Solution {
+    public List<String> findItinerary(String[][] tickets) {
+        Map<String, PriorityQueue<String>> map = new HashMap<>();
+        LinkedList<String> res = new LinkedList<>();
+        for (String[] ticket : tickets) {
+            map.computeIfAbsent(ticket[0], queue -> new PriorityQueue<>()).add(ticket[1]);
+        }
+        
+        String cur = "JFK";
+        Stack<String> stack = new Stack<>();
+        
+        for (int i = 0; i < tickets.length ;i++) {
+            // Invalid path
+            while (!map.containsKey(cur) || map.get(cur).isEmpty()) {
+                stack.push(cur);
+                cur = res.remove(res.size()-1);
+            }
+            res.add(cur);
+            cur = map.get(cur).poll();
+        }
+        res.add(cur);
+        while(!stack.isEmpty()) res.add(stack.pop());
+        return res;
+    }
+}
+
+// DFS
 class Solution {
     public List<String> findItinerary(String[][] tickets) {
         Map<String, PriorityQueue<String>> map = new HashMap<>();

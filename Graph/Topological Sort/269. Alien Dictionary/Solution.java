9a7a71c3025603f4487d8bd8085027bb6a87edab
teng <yangteng0813@gmail.com>
2018-05-13 17:54:10 -0400

    5.13

--- Graph/Topological Sort/269. Alien Dictionary/Solution.java
+++ Graph/Topological Sort/269. Alien Dictionary/Solution.java
@@ -1,61 +1,60 @@
 class Solution {
     public String alienOrder(String[] words) {
-        Map<Character, Set<Character>> map=new HashMap<Character, Set<Character>>();
-        Map<Character, Integer> degree = new HashMap<Character, Integer>();
-        StringBuilder sb = new StringBuilder();
-
-        initIndegree(words,degree);         
-        updateMapAndDegree(words,map,degree); 
-        return topoSort(words,sb,map,degree);
+        Map<Character, Set<Character>> map = new HashMap<>();
+        Map<Character, Integer> degree = new HashMap<>();
+        initIndegree(words, degree);
+        updateMapAndDegree(words, map, degree);
+        return getResult(map, degree);
 
     }
-    
-    private void initIndegree(String[] words,Map<Character,Integer> degree){
-        for(String s: words){
-            for(char c: s.toCharArray()){
-                degree.put(c,0);
+
+    private void initIndegree(String[] words, Map<Character, Integer> degree) {
+        for (String s : words) {
+            for (char c : s.toCharArray()) {
+                degree.put(c, 0);
             }
         }
     }
-    
-    private void updateMapAndDegree(String[] words,Map<Character, Set<Character>> map,Map<Character,Integer> degree){
-        for(int i = 0; i < words.length-1; i++){
+
+    private void updateMapAndDegree(String[] words, Map<Character, Set<Character>> map, Map<Character, Integer> degree) {
+        for (int i = 0; i < words.length - 1; i++) {
             String one = words[i];
-            String two = words[i+1];
+            String two = words[i + 1];
             int length = Math.min(one.length(), two.length());
-            for(int j = 0 ; j < length; j++){
+            for (int j = 0; j < length; j++) {
                 char ch1 = one.charAt(j);
                 char ch2 = two.charAt(j);
-                if( ch1 != ch2){
-                    Set<Character> set = new HashSet<Character>();
-                    if(map.containsKey(ch1)) set = map.get(ch1);
-                    if(!set.contains(ch2)){
+                if (ch1 != ch2) {
+                    Set<Character> set = new HashSet<>();
+                    if (map.containsKey(ch1)) set = map.get(ch1);
+                    if (!set.contains(ch2)) {
                         set.add(ch2);
                         map.put(ch1, set);
-                        degree.put(ch2, degree.get(ch2)+1);
+                        degree.put(ch2, degree.get(ch2) + 1);
                     }
                     break;
                 }
             }
         }
     }
-    
-    private String topoSort(String[] words, StringBuilder sb, Map<Character, Set<Character>> map,Map<Character,Integer> degree){
-        Queue<Character> queue =new LinkedList<Character>();
-        for(char ch: degree.keySet()){
-            if(degree.get(ch)==0) queue.add(ch);
+
+    private String getResult(Map<Character, Set<Character>> map, Map<Character, Integer> degree) {
+        StringBuilder sb = new StringBuilder();
+        Queue<Character> queue = new LinkedList<>();
+        for (char ch : degree.keySet()) {
+            if (degree.get(ch) == 0) queue.add(ch);
         }
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             char ch = queue.remove();
             sb.append(ch);
-            if(map.containsKey(ch)){
-                for(char ch2: map.get(ch)){
-                    degree.put(ch2,degree.get(ch2)-1);
-                    if(degree.get(ch2) == 0) queue.add(ch2);
+            if (map.containsKey(ch)) {
+                for (char ch2 : map.get(ch)) {
+                    degree.put(ch2, degree.get(ch2) - 1);
+                    if (degree.get(ch2) == 0) queue.add(ch2);
                 }
             }
         }
-        if( sb.length() != degree.size()) return "";
+        if (sb.length() != degree.size()) return "";
         return sb.toString();
     }
 }

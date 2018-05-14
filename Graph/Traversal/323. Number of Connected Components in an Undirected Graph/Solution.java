ea3cebf1eb58587ff1f88aa2a427a0e71d2226f6
teng <yangteng0813@gmail.com>
2018-05-13 20:37:40 -0400

    5.13

--- Graph/Traversal/323. Number of Connected Components in an Undirected Graph/Solution.java
+++ Graph/Traversal/323. Number of Connected Components in an Undirected Graph/Solution.java
@@ -1,25 +1,25 @@
 public class Solution {
     public int countComponents(int n, int[][] edges) {
         int res = 0;
-        Map<Integer,List<Integer>> map = new HashMap<>();
-        Set<Integer> set = new HashSet<>();
+        Map<Integer,List<Integer>> map = new HashMap<>();        
         for( int i = 0 ; i < n ; i++) map.put(i, new ArrayList<>());
-        
-        for( int[] e : edges){
-            map.get(e[0]).add(e[1]);
-            map.get(e[1]).add(e[0]);    
+        for( int[] edge : edges){            
+            map.get(edge[0]).add(edge[1]);
+            map.get(edge[1]).add(edge[0]);    
         }
+        Set<Integer> set = new HashSet<>();
         for (int i = 0; i < n; i++) {
             if (set.add(i)) {
-                helper(i, map, set);
+                dfs(i, map, set);
                 res++;
             }
         }
         return res;
     }
-    private void helper(int i, Map<Integer, List<Integer>> map, Set<Integer> set) {
-        for (int j : map.get(i)) {
-            if (set.add(j)) helper(j, map, set);    
+    
+    private void dfs (int cur, Map<Integer, List<Integer>> map, Set<Integer> set) {
+        for (int i : map.get(cur)) {
+            if (set.add(i)) dfs(i, map, set);    
         }
     }
 }

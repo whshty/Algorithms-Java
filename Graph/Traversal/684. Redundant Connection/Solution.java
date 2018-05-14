504df61a2200bb953a51a670d1174055750b7b69
teng <yangteng0813@gmail.com>
2018-05-14 11:53:36 -0400

    5.14

--- Graph/Traversal/684. Redundant Connection/Solution.java
+++ Graph/Traversal/684. Redundant Connection/Solution.java
@@ -1,21 +1,19 @@
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
-        int[] union = new int[2001];
-        for( int i = 0 ; i < union.length ; i++ ) union[i] = i;
+        int[] nums = new int[2001];
+        for (int i = 0 ; i < nums.length ; i++) nums[i] = i;
         
-        for( int[] edge : edges ){
+        for (int[] edge : edges) {
             int edge1 = edge[0] , edge2 = edge[1];
-            int id1 = find(union,edge1), id2 = find(union,edge2);
+            int id1 = find(nums,edge1), id2 = find(nums,edge2);
             if( id1 == id2 ) return edge;
-            else union[id2] = id1;
+            else nums[id2] = id1;
         }
         return new int[2];
     }
     
-    public int find(int[] union, int id){
-        if( union[id] != id){
-            union[id] = find(union,union[id]);
-        }
-        return union[id];
+    private int find (int[] nums, int id){
+        if (nums[id] != id) nums[id] = find(nums, nums[id]);
+        return nums[id];
     }
 }

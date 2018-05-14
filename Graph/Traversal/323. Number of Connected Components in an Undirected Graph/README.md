f623fdd9560b8d4b2293a3c9b28b10b73bc1810d
teng <yangteng0813@gmail.com>
2018-05-13 21:01:51 -0400

    5.13

--- Graph/Traversal/323. Number of Connected Components in an Undirected Graph/README.md
+++ Graph/Traversal/323. Number of Connected Components in an Undirected Graph/README.md
@@ -27,3 +27,40 @@ public class Solution {
     }
 }
 ```
+
+## BFS
+
+```java
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        int res = 0;
+        Map<Integer,List<Integer>> map = new HashMap<>();        
+        for (int i = 0 ; i < n ; i++) map.put(i, new ArrayList<>());
+        for (int[] edge : edges){            
+            map.get(edge[0]).add(edge[1]);
+            map.get(edge[1]).add(edge[0]);    
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0 ; i < n ; i++) {
+            if (!set.contains(i)){
+                res++;
+                bfs(map, set, i);
+                
+            }
+        }
+        return res;
+    }
+    
+    private void bfs(Map<Integer,List<Integer>> map, Set<Integer> set, int cur) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(cur);
+        while (!queue.isEmpty()) {
+            int i = queue.poll();
+            set.add(i);
+            for (int next : map.get(i)){
+                if(!set.contains(next)) queue.add(next);
+            }
+        }
+    }
+}
+```
--- Graph/Traversal/323. Number of Connected Components in an Undirected Graph/Solution.java
+++ Graph/Traversal/323. Number of Connected Components in an Undirected Graph/Solution.java
@@ -1,4 +1,5 @@
-public class Solution {
+// BFS 
+class Solution {
     public int countComponents(int n, int[][] edges) {
         int res = 0;
         Map<Integer,List<Integer>> map = new HashMap<>();        
@@ -20,6 +21,40 @@ public class Solution {
     private void dfs(int cur, Map<Integer, List<Integer>> map, Set<Integer> set) {
         for (int i : map.get(cur)) {
             if (set.add(i)) dfs(i, map, set);    
+        }
+    }
+}
+
+// DFS
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        int res = 0;
+        Map<Integer,List<Integer>> map = new HashMap<>();        
+        for (int i = 0 ; i < n ; i++) map.put(i, new ArrayList<>());
+        for (int[] edge : edges){            
+            map.get(edge[0]).add(edge[1]);
+            map.get(edge[1]).add(edge[0]);    
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0 ; i < n ; i++) {
+            if (!set.contains(i)){
+                res++;
+                bfs(map, set, i);
+                
+            }
+        }
+        return res;
+    }
+    
+    private void bfs(Map<Integer,List<Integer>> map, Set<Integer> set, int cur) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(cur);
+        while (!queue.isEmpty()) {
+            int i = queue.poll();
+            set.add(i);
+            for (int next : map.get(i)){
+                if(!set.contains(next)) queue.add(next);
+            }
         }
     }
 }

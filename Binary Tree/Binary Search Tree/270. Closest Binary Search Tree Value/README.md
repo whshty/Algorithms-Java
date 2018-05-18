4ea5a16974a92de4a2aeb987b13e6e9829e2c6e2
teng <yangteng0813@gmail.com>
2018-05-18 19:25:06 -0400

    5.18

--- Binary Tree/Binary Search Tree/270. Closest Binary Search Tree Value/README.md
+++ Binary Tree/Binary Search Tree/270. Closest Binary Search Tree Value/README.md
@@ -20,15 +20,13 @@ class Solution {
 ## 2.Iteration
 ```java
 class Solution {
-    public int closestValue(TreeNode root, double target) {  
-        int res = root.val;
-        while( root != null ){
-            // Update cloest value
-            if( Math.abs(root.val-target) < Math.abs(res-target)) res = root.val;
-            if( root.val > target ) root = root.left;
-            else root = root.right;
-        }
-        return res;
+    public int closestValue(TreeNode node, double target) {        
+        int cur = node.val;
+        node = target < cur ? node.left : node.right;
+        if (node == null || target == cur) return cur;
+        int nextVal = closestValue(node, target);
+        
+        return Math.abs(nextVal - target) < Math.abs(cur - target) ? next:cur;     
     }
 }
 ```
--- Binary Tree/Binary Search Tree/270. Closest Binary Search Tree Value/Solution.java
+++ Binary Tree/Binary Search Tree/270. Closest Binary Search Tree Value/Solution.java
@@ -1,16 +1,16 @@
+// BFS
 class Solution {
-    public int closestValue(TreeNode root, double target) {
-        int cur = root.val;
+    public int closestValue(TreeNode node, double target) {        
+        int cur = node.val;
+        node = target < cur ? node.left : node.right;
+        if (node == null || target == cur) return cur;
+        int nextVal = closestValue(node, target);
         
-        if( target == cur ) return cur;
-        root = target < cur ? root.left : root.right;
-        if( root == null ) return cur;
-        int next = closestValue(root,target);
-        
-        return Math.abs( next - target ) < Math.abs( cur - target) ?
-                next : cur;     
+        return Math.abs(nextVal - target) < Math.abs(cur - target) ? next:cur;     
     }
 }
+
+// DFS
 class Solution {
     public int closestValue(TreeNode root, double target) {  
         int res = root.val;

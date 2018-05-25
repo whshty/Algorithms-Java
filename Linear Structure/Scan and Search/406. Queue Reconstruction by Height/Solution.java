9ee94fb1fbbb0c4a45ebe031f5dc0a19440aa297
teng <yangteng0813@gmail.com>
2018-05-24 20:35:04 -0400

    5.24

--- Linear Structure/Scan and Search/406. Queue Reconstruction by Height/Solution.java
+++ Linear Structure/Scan and Search/406. Queue Reconstruction by Height/Solution.java
@@ -3,7 +3,7 @@ class Solution {
         Arrays.sort(people, (a, b) -> a[0] != b[0] ? b[0] - a[0] : a[1] - b[1]);
         
         List<int[]> res = new ArrayList<>();
-        for( int[] cur : people ){
+        for (int[] cur : people) {
             // add at position cur[1]
             res.add(cur[1],cur);
         }

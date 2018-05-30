c9358a4f7037d1b18c326318a66bcaff583f4d8b
teng <yangteng0813@gmail.com>
2018-05-29 20:25:59 -0400

    5.29

--- Memorized Search and DP /2D/97. Interleaving String/Solution.java
+++ Memorized Search and DP /2D/97. Interleaving String/Solution.java
@@ -1,28 +1,3 @@
-## DP
-1. Last step
-	* The last character is either from A[m-1] or B[n-1]
-	* If it's from A[m-1], then X[0,m+n-2] is built by
-		A[0,m-2] and B[0,n-1]
-	* If it' sfrom B[n-1], then X[0,m+n-2] is built by
-		A[0,m-1] and B[0,n-2]
-
-2. Transfer function
-	* State : dp[s][i][j]. The first s characters can be built by A[0,i-1] and B[0,j-1], s = i + j
-	* dp[i][j] = (dp[i-1][j] && X[i+j-1] == A[i-1]) || (dp[i][j-1] && d[i+j-1] )
-
-3. Initial and boundary conditions
-	* dp[0][0] = true
-	* if i = 0, we only consider B
-	* if j = 0, we only consider A
-
-4. Calculation order
-	* dp[0][0].....dp[0][n]
-	* dp[1][0].....dp[1][n]
-	* dp[m][0].....dp[m][n]
-	* return dp[m][n]
-
-	
-```java
 class Solution {
     public boolean isInterleave(String a, String b, String x) {
         int m = a.length(), n = b.length();
@@ -47,4 +22,3 @@ class Solution {
         return dp[m][n];   
     }
 }
-```

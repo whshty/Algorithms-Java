ceb7d0a212f7b873367868475b2dd8629c4c5558
teng <yangteng0813@gmail.com>
2018-05-29 22:20:45 -0400

    5.29

--- Memorized Search and DP /2D/115. Distinct Subsequences/Solution.java
+++ Memorized Search and DP /2D/115. Distinct Subsequences/Solution.java
@@ -1,22 +1,3 @@
-## DP
-1. Last step and status
-	1. B[m-1] == A[n-1] 
-	2. B[m-1] and A[n-1] is not a pair, calculate how many times B[0,n-1] appears in A[0,m-2]
-	3. dp[i][j], the times of B[0,j-1] appears in A[0,i-1]
-
-2. Transfer function
-	* dp[i][j] = dp[i-1][j-1] | A[i-1] = B[j-1] + dp[i-1][j]
-
-3. Initial and boundary conditions
-	* If b is empty, B appears 1 time in A, dp[i][0] = 1
-	* If a is empty, A appears 0 time in B, dp[0][j] = 0
-
-4. Calculation order
-	* 0 to n and 0 to m
-	* return dp[m][n]
-
-	
-```java
 class Solution {
     public int numDistinct(String a, String b) {
         int m = a.length();
@@ -40,4 +21,3 @@ class Solution {
         return dp[m][n];
     }
 }
-```

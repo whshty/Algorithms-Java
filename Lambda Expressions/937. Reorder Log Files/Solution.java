add076e201d60c0d6ed7fcc580b74e27547c2999
Teng Yang <yangteng0813@gmail.com>
2019-05-30 19:50:41 -0400

    Removed OA

---
+++ Lambda Expressions/937. Reorder Log Files/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        Arrays.sort(logs, (log1, log2) -> {
+            String[] str1 = log1.split(" ", 2);
+            String[] str2 = log2.split(" ", 2);
+            boolean isDigit1 = Character.isDigit(str1[1].charAt(0));
+            boolean isDigit2 = Character.isDigit(str2[1].charAt(0));
+            if (!isDigit1 && !isDigit2) {
+                int temp = str1[1].compareTo(str2[1]);
+                if (temp != 0) return temp;
+                return str1[0].compareTo(str2[0]);
+            }
+            return isDigit1 ? (isDigit2 ? 0 : 1) : -1;
+        });
+        return logs;
+    }
+}
